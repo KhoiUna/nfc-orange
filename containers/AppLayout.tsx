1317b1cde834ac57c7c4273afaf222f73e177b82
KhoiNguyen <thorwaitson@gmail.com>
2022-09-29 22:50:51 -0500

    add profile.tsx page

---
+++ containers/AppLayout.tsx
@@ -0,0 +1,53 @@
+import Head from "next/head";
+import { ReactNode, useEffect } from "react";
+import AppHeaderBar from "../components/ui/AppHeaderBar";
+import useAuth from "../lib/useAuth";
+
+interface LayoutProps {
+  children: ReactNode | ReactNode[];
+  title: string;
+}
+
+const AppLayout = ({ children, title }: LayoutProps) => {
+  const pageTitle = `nTap | ${title}`;
+
+  const { data } = useAuth({});
+  const isAuthenticated = data?.isAuthenticated;
+
+  if (!isAuthenticated)
+    return (
+      <Head>
+        <meta name="description" content="nTap | NFC for Professionals" />
+        <link rel="icon" href="/favicon.png" />
+        <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+
+        <title>nTap | ...</title>
+      </Head>
+    );
+
+  return (
+    <>
+      <Head>
+        <meta name="description" content="nTap | NFC for Professionals" />
+        <link rel="icon" href="/favicon.png" />
+        <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+
+        <title>{pageTitle}</title>
+      </Head>
+
+      <AppHeaderBar
+        title={title}
+        navLinks={[
+          {
+            href: "/api/logout",
+            text: "Logout",
+          },
+        ]}
+      />
+
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default AppLayout;
---
+++ pages/profile.tsx
@@ -0,0 +1,9 @@
+import AppLayout from "../containers/AppLayout";
+
+export default function Profile() {
+  return (
+    <AppLayout title="Profile">
+      <h1>profile</h1>
+    </AppLayout>
+  );
+}
