fc36845e8461a9f197030d58ea7863a6d34f4f77
KhoiNguyen <thorwaitson@gmail.com>
2022-09-29 22:15:10 -0500

    update register function and page

---
+++ pages/api/register.ts
@@ -0,0 +1,95 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import Joi from "joi";
+import { RegisterInfo } from "../register";
+import client from "../../db/client";
+import PasswordHelper from "../../helpers/PasswordHelper";
+
+export type ApiResponse = {
+  success: any;
+  error: any;
+};
+
+const isValid = async (registerInfo: RegisterInfo) => {
+  const schema = Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+    confirm_password: Joi.ref("password"),
+  });
+
+  try {
+    const value = await schema.validateAsync(registerInfo);
+  } catch (error: any) {
+    if (error.details[0].type === "any.only")
+      return {
+        success: false,
+        error: "Confirm password does not match",
+      };
+
+    return { success: false, error: error.details[0].message };
+  }
+
+  return { success: true, error: false };
+};
+
+export default async function register(
+  req: NextApiRequest,
+  res: NextApiResponse<ApiResponse>
+) {
+  try {
+    if (req.method !== "POST")
+      return res
+        .status(405)
+        .json({ success: false, error: "Method not allowed" });
+
+    const { email, password } = req.body;
+    const { c_id } = req.query;
+
+    // Validate email & password
+    const { success, error } = await isValid(req.body);
+    if (error) return res.status(400).json({ success: false, error });
+
+    // Validate card id c_id
+    if (!c_id)
+      return res
+        .status(400)
+        .json({ success: false, error: "Invalid registration" });
+
+    const { rows: cards } = await client.query(
+      "SELECT * FROM cards WHERE uuid = $1;",
+      [c_id]
+    );
+    const cardId = cards[0].id;
+
+    if (cards.length !== 1)
+      return res
+        .status(400)
+        .json({ success: false, error: "Invalid registration" });
+
+    // Validate unique user
+    const { rows: users } = await client.query(
+      "SELECT * FROM users WHERE email = $1;",
+      [email]
+    );
+    if (users.length !== 0)
+      return res
+        .status(400)
+        .json({ success: false, error: "Email already registered" });
+
+    // Save user to db
+    const savingUserResponse = await client.query(
+      "INSERT INTO users(email, password, updated_at, card_id) VALUES ($1, $2, $3, $4);",
+      [email, await PasswordHelper.hashPassword(password), new Date(), cardId]
+    );
+
+    if (!savingUserResponse) throw new Error("Error creating user");
+
+    return res
+      .status(200)
+      .json({ success: "Registered successfully", error: false });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ success: false, error: "Internal server error" });
+  }
+}
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,17 +1,45 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { SyntheticEvent, useState } from "react";
+import TextLoader from "../components/ui/TextLoader";
 import Layout from "../containers/Layout";
 
-export default function Register() {
-  const [registerInfo, setRegisterInfo] = useState({
-    email: "",
-    password: "",
-    confirm_password: "",
+export type RegisterInfo = {
+  email: string;
+  password: string;
+  confirm_password: string;
+};
+
+const registerInfoInitialState = {
+  email: "",
+  password: "",
+  confirm_password: "",
+};
+
+interface RegisterProps {
+  showForm: boolean;
+}
+
+export default function Register({ showForm }: RegisterProps) {
+  const router = useRouter();
+
+  const [registerInfo, setRegisterInfo] = useState<RegisterInfo>(
+    registerInfoInitialState
+  );
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [status, setStatus] = useState({
+    error: false,
+    text: "",
   });
 
   const handleChange = (event: SyntheticEvent) => {
     const target = event.target as HTMLInputElement;
-
+    setStatus({
+      error: false,
+      text: "",
+    });
     setRegisterInfo((prev) => ({
       ...prev,
       [target.name]: target.value,
@@ -21,9 +49,10 @@ export default function Register() {
   const handleSubmit = async (event: SyntheticEvent) => {
     try {
       event.preventDefault();
+      setIsLoading(true);
 
       const { success, error } = await (
-        await fetch("/api/register", {
+        await fetch(`/api/register?c_id=${router.query.c_id}`, {
           method: "POST",
           headers: new Headers({
             "content-type": "application/json",
@@ -34,12 +63,42 @@ export default function Register() {
 
       if (error) throw error;
 
+      setStatus({
+        error: false,
+        text: success,
+      });
+      setRegisterInfo(registerInfoInitialState);
+      setIsLoading(false);
+
       return true;
-    } catch (error) {
+    } catch (error: any) {
       console.error("Error registering");
+      setIsLoading(false);
+      setStatus({
+        error: true,
+        text: error,
+      });
       return false;
     }
   };
+
+  if (!showForm)
+    return (
+      <Layout title="Register">
+        <div className="text-center bg-slate-50 p-20 m-auto min-h-[80vh]">
+          <h1 className="text-[2.5rem] font-bold mx-auto my-3">Register</h1>
+          <h2 className="text-[1.8rem]">You should get our NFC card first</h2>
+
+          <div className="my-2">
+            <Link href={"/login"}>
+              <a className="text-[1.3rem] underline text-primary font-bold">
+                Login
+              </a>
+            </Link>
+          </div>
+        </div>
+      </Layout>
+    );
 
   return (
     <Layout title="Register">
@@ -83,15 +142,26 @@ export default function Register() {
 
             <div className="my-5 drop-shadow-lg">
               <button
-                className="bg-primary text-[1.3rem] text-white rounded-lg p-2 border border-black"
+                className="bg-primary text-[1.3rem] text-white rounded-lg p-2 border border-black flex m-auto"
                 type="submit"
               >
-                Submit
+                {!isLoading && "Submit"}
+                {isLoading && <TextLoader loadingText="Submitting" />}
               </button>
+
+              {status.text && (
+                <p
+                  className={`${
+                    status.error === true ? "text-red-800" : "text-green-800"
+                  } text-[1.3rem] p-2 font-bold my-1`}
+                >
+                  {status.text}
+                </p>
+              )}
             </div>
           </form>
 
-          <div className="my-5">
+          <div>
             <Link href={"/login"}>
               <a className="text-[1.3rem] underline text-primary font-bold">
                 Login
@@ -102,4 +172,20 @@ export default function Register() {
       </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps(context: any) {
+  // Only show form if c_id query exists in URL
+  if (!context.query.c_id)
+    return {
+      props: {
+        showForm: false,
+      },
+    };
+
+  return {
+    props: {
+      showForm: true,
+    },
+  };
 }
