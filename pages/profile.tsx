301aa2ca45a6afcf4c921af94407dc32fdbaaefc
KhoiNguyen <thorwaitson@gmail.com>
2022-09-29 22:58:16 -0500

    update profile.tsx page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -2,6 +2,8 @@ import AppLayout from "../containers/AppLayout";
 import { Icon } from "@iconify/react";
 
 export default function Profile() {
+  // TODO: upload to Firebase
+
   return (
     <AppLayout title="Profile">
       <button className="text-[1.8rem] bg-blue-100 rounded-lg p-3 flex drop-shadow-lg m-auto my-4 text-blue-800 active:drop-shadow-none">
