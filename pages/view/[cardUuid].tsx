88df1ad2afbb7d8310ece456f76b5524187ec7ab
KhoiNguyen <thorwaitson@gmail.com>
2022-09-29 22:02:01 -0500

    add new page view

---
+++ pages/view/[cardUuid].tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Layout from "../../containers/Layout";
+
+export default function View() {
+  return (
+    <Layout title="View">
+      <div className="text-center py-[20vh] min-h-[80vh]">
+        <h1 className="text-[5rem] font-bold">nTap</h1>
+        <h2 className="text-[2rem]">NFC for Professionals</h2>
+
+        <div>
+          <Link href={"/register"}>
+            <a className="text-[1.3rem] underline text-primary font-bold">
+              Register
+            </a>
+          </Link>
+
+          <Link href={"/register"}>
+            <a className="text-[1.3rem] ml-10 underline text-primary font-bold">
+              Login
+            </a>
+          </Link>
+        </div>
+      </div>
+    </Layout>
+  );
+}
