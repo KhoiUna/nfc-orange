ed0efe460bd281d9a0b58aa373ac06372f41db70
KhoiNguyen <thorwaitson@gmail.com>
2022-09-29 22:57:51 -0500

    update profile.tsx page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,9 +1,16 @@
 import AppLayout from "../containers/AppLayout";
+import { Icon } from "@iconify/react";
 
 export default function Profile() {
   return (
     <AppLayout title="Profile">
-      <h1>profile</h1>
+      <button className="text-[1.8rem] bg-blue-100 rounded-lg p-3 flex drop-shadow-lg m-auto my-4 text-blue-800 active:drop-shadow-none">
+        Upload your resume
+        <Icon
+          className="text-[2.4rem] ml-2"
+          icon="ant-design:upload-outlined"
+        />
+      </button>
     </AppLayout>
   );
 }
