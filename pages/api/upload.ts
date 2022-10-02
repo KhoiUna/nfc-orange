d05b9f42feaf01b78848dc824d26153888efc6ca
KhoiNguyen <thorwaitson@gmail.com>
2022-10-02 01:03:26 -0500

    update upload.ts api route

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,32 +1,32 @@
+import { withIronSessionApiRoute } from "iron-session/next";
 import { NextApiRequest, NextApiResponse } from "next";
+import client from "../../db/client";
+import { sessionOptions } from "../../lib/session";
 import { ApiResponse } from "./register";
-import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
 
-export const firebaseConfig = {
-  apiKey: process.env.firebase_apiKey,
-  authDomain: process.env.firebase_authDomain,
-  projectId: process.env.firebase_projectId,
-  storageBucket: process.env.firebase_storageBucket,
-  messagingSenderId: process.env.firebase_messagingSenderId,
-  appId: process.env.firebase_appId,
-  measurementId: process.env.firebase_measurementId,
-};
-
-export async function upload(
-  req: NextApiRequest,
-  res: NextApiResponse<ApiResponse>
-) {
+async function upload(req: NextApiRequest, res: NextApiResponse<ApiResponse>) {
   try {
-    // TODO: upload to Firebase
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
+    if (req.method !== "POST")
+      return res
+        .status(403)
+        .json({ success: false, error: "Method not allowed" });
 
-    // Initialize Cloud Storage and get a reference to the service
-    const storage = getStorage(app);
+    const { fileURL } = req.body;
 
-    return res.json({ success: true, error: false });
+    // Save link to database
+    const response = await client.query(
+      "INSERT INTO links(user_id, url, updated_at) VALUES ((SELECT id FROM users WHERE email = $1), $2, $3) ON CONFLICT (user_id) DO UPDATE SET url = $2, updated_at = $3;",
+      [req.session.user?.email, fileURL, new Date()]
+    );
+    if (!response) throw "Error saving link";
+
+    return res.status(200).json({ success: true, error: false });
   } catch (error) {
     console.error(error);
+    return res
+      .status(500)
+      .json({ success: false, error: "Internal server error" });
   }
 }
+
+export default withIronSessionApiRoute(upload, sessionOptions);
