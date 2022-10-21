b69e9e2cf8961ebcf56d15e0c9a7fc7d6aabf979
KhoiNguyen <thorwaitson@gmail.com>
2022-10-21 01:24:05 -0500

    update view page

--- pages/view/[cardUuid].tsx
+++ pages/view/[cardUuid].tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { isAndroid } from "react-device-detect";
+// import { isAndroid } from "react-device-detect";
 import useSWR from "swr";
 import TextLoader from "../../components/ui/TextLoader";
 import Layout from "../../containers/Layout";
 import { swrFetcher } from "../../lib/swrFetcher";
-// import { getDocument, GlobalWorkerOptions } from "pdfjs-dist";
-// import { useEffect } from "react";
-// import { RenderParameters } from "pdfjs-dist/types/src/display/api";
+import { getDocument, GlobalWorkerOptions } from "pdfjs-dist";
+import { useEffect, useState } from "react";
+import { RenderParameters } from "pdfjs-dist/types/src/display/api";
 
 export default function View() {
   const router = useRouter();
@@ -15,55 +15,57 @@ export default function View() {
 
   const { data } = useSWR(cardUuid && `/api/view?c_id=${cardUuid}`, swrFetcher);
 
-  // TODO: Remove pdfjs code
-  // useEffect(() => {
-  //   if (data?.success.length > 0) {
-  //     const url =
-  //       "https://drive.google.com/viewerng/viewer?embedded=true&url=https://firebasestorage.googleapis.com/v0/b/ntap-6cc7b.appspot.com/o/resumes%2Ffa5cd5b9-84f2-46cb-a4b4-c806947893e6.pdf?alt=media&token=19362fd3-34ba-4f02-ad8f-356c66c82563" ||
-  //       data.success[0].url;
+  const [maxPagesInPDF, setMaxPagesInPDF] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
 
-  //     GlobalWorkerOptions.workerSrc =
-  //       "https://cdn.jsdelivr.net/npm/pdfjs-dist@2.16.105/build/pdf.worker.js";
+  useEffect(() => {
+    if (data?.success.length > 0) {
+      const pdfURL = data.success[0].url;
 
-  //     const loadingTask = getDocument(url);
-  //     loadingTask.promise.then(
-  //       function (pdf) {
-  //         console.log("PDF loaded");
+      GlobalWorkerOptions.workerSrc =
+        "https://cdn.jsdelivr.net/npm/pdfjs-dist@2.16.105/build/pdf.worker.js";
 
-  //         // Fetch the first page
-  //         const pageNumber = 1;
-  //         pdf.getPage(pageNumber).then(function (page) {
-  //           console.log("PDF page loaded");
+      const loadingTask = getDocument(pdfURL);
+      loadingTask.promise.then(
+        function (pdf) {
+          console.log("PDF loaded");
 
-  //           const scale = 1.5;
-  //           const viewport = page.getViewport({ scale });
+          // Set pagesInPDF equal to number of pages in PDF
+          setMaxPagesInPDF(pdf.numPages);
 
-  //           // Prepare canvas using PDF page dimensions
-  //           const canvas = document.getElementById(
-  //             "the-canvas"
-  //           ) as HTMLCanvasElement;
-  //           const context = canvas.getContext("2d");
-  //           canvas.height = viewport.height;
-  //           canvas.width = viewport.width;
+          // Fetch the first page
+          pdf.getPage(currentPage).then(function (page) {
+            console.log("PDF page loaded");
 
-  //           // Render PDF page into canvas context
-  //           const renderContext = {
-  //             canvasContext: context,
-  //             viewport: viewport,
-  //           };
-  //           const renderTask = page.render(renderContext as RenderParameters);
-  //           renderTask.promise.then(function () {
-  //             console.log("PDF rendered");
-  //           });
-  //         });
-  //       },
-  //       function (error) {
-  //         // PDF loading error
-  //         console.error("Error rendering PDF");
-  //       }
-  //     );
-  //   }
-  // }, [data]);
+            const scale = 2.1;
+            const viewport = page.getViewport({ scale });
+
+            // Prepare canvas using PDF page dimensions
+            const canvas = document.getElementById(
+              "the-canvas"
+            ) as HTMLCanvasElement;
+            const context = canvas.getContext("2d");
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+
+            // Render PDF page into canvas context
+            const renderContext = {
+              canvasContext: context,
+              viewport: viewport,
+            };
+            const renderTask = page.render(renderContext as RenderParameters);
+            renderTask.promise.then(function () {
+              console.log("PDF rendered");
+            });
+          });
+        },
+        function (error) {
+          // PDF loading error
+          console.error("Error rendering PDF");
+        }
+      );
+    }
+  }, [data, currentPage]);
 
   if (!data)
     return (
@@ -108,38 +110,73 @@ export default function View() {
       </Layout>
     );
 
+  // TODO: remove redirecting on Android
   // if (isAndroid)
-  return (
-    <Layout title="View">
-      <div className="text-center py-[20vh] min-h-[80vh]">
-        <meta
-          http-equiv="refresh"
-          content={`0; url = https://drive.google.com/viewerng/viewer?embedded=true&url=${encodeURI(
-            success[0].url
-          )}`}
-        />
-      </div>
-    </Layout>
-  );
-
   //   return (
   //     <Layout title="View">
   //       <div className="text-center py-[20vh] min-h-[80vh]">
-  //         {success.map((item: { url: string }) => (
-  //           <div className="" key={item.url}>
-  //             <object
-  //               className="w-screen h-[70vh] m-auto"
-  //               type="application/pdf"
-  //               data={`https://drive.google.com/viewerng/viewer?embedded=true&url=${encodeURI(
-  //                 item.url
-  //               )}`}
-  //             />
-  //           </div>
-  //         ))}
-
-  //         {/* TODO: remove code */}
-  //         {/* <canvas id="the-canvas"></canvas> */}
+  //         <meta
+  //           http-equiv="refresh"
+  //           content={`0; url = https://drive.google.com/viewerng/viewer?embedded=true&url=${encodeURI(
+  //             success[0].url
+  //           )}`}
+  //         />
   //       </div>
   //     </Layout>
   //   );
+
+  const handleClick = (action: "back" | "next") => {
+    if (currentPage <= 1 && action === "back") return;
+    if (action === "next" && currentPage >= maxPagesInPDF) return;
+
+    if (action === "back") {
+      setCurrentPage(currentPage - 1);
+    }
+    if (action === "next") {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  return (
+    <Layout title="View">
+      <div className="text-center py-4 min-h-[80vh] bg-slate-100">
+        <div className="flex justify-center m-4">
+          <div className="flex items-center">
+            <button
+              className="bg-primary p-2 rounded-lg drop-shadow-lg"
+              onClick={() => handleClick("back")}
+            >
+              <span className="text-xl text-white">Back</span>
+            </button>
+            <div>
+              <p className="mx-6 text-xl bg-white px-3 py-2 rounded-lg border-slate-400 border-2 h-fit">
+                {currentPage}
+              </p>
+            </div>
+            <button
+              className="bg-primary p-2 rounded-lg drop-shadow-lg"
+              onClick={() => handleClick("next")}
+            >
+              <span className="text-xl text-white">Next</span>
+            </button>
+          </div>
+        </div>
+
+        {success.map((item: { url: string }) => (
+          <div className="max-w-[100%] overflow-auto m-4" key={item.url}>
+            {/* TODO: remove old Google PDF Viewer  */}
+            {/* <object
+              className="w-screen h-[70vh] m-auto"
+              type="application/pdf"
+              data={`https://drive.google.com/viewerng/viewer?embedded=true&url=${encodeURI(
+                item.url
+              )}`}
+            /> */}
+
+            <canvas id="the-canvas" className="m-auto w-[1000px]"></canvas>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  );
 }
