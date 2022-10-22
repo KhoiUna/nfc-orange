d9186dfe19c587650535c8c94968bde188ffe08f
KhoiNguyen <thorwaitson@gmail.com>
2022-10-22 11:57:47 -0500

    add analytics script

--- containers/ViewLayout.tsx
+++ containers/ViewLayout.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import Script from "next/script";
 import { ReactNode } from "react";
 import Logo from "../components/Logo";
 import { HEADER_BAR_BG_COLOR } from "../components/ui/HeaderBar";
@@ -21,6 +22,30 @@ const ViewLayout = ({ children, title }: LayoutProps) => {
 
         <title>{pageTitle}</title>
       </Head>
+
+      {/* Google tag (gtag.js)  */}
+      <Script
+        async
+        strategy="afterInteractive"
+        src={"https://www.googletagmanager.com/gtag/js?id=G-JHYW0T0JLC"}
+      />
+      <Script
+        id="google_analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-JHYW0T0JLC');`,
+        }}
+      />
+
+      {/* umami analytics */}
+      <Script
+        async
+        defer
+        data-website-id="47dbc34e-c03e-482a-8aef-2a0a8dd9fe90"
+        src="https://umami.khoiuna.info/umami.js"
+      />
 
       <div className={`bg-slate-200 sm:w-screen w-max`}>
         <header
