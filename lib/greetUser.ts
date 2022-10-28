ece76d5ad2fd509b94740a6cf6cd8c3136aa63f5
KhoiNguyen <thorwaitson@gmail.com>
2022-10-28 16:01:28 -0500

    update greetUser.ts

--- lib/greetUser.ts
+++ lib/greetUser.ts
@@ -2,13 +2,13 @@ const greetUser = (name: string): string => {
   const hour = new Date().getHours();
   let greet = "";
   if (hour >= 6 && hour <= 11) {
-    greet = "Good morning 🌄, ";
+    greet = "Good morning 🌄 ";
   } else if (hour >= 12 && hour <= 17) {
-    greet = "Good afternoon ☀️, ";
+    greet = "Good afternoon ☀️ ";
   } else if (hour >= 18 && hour <= 24) {
-    greet = "Good evening 🌃, ";
+    greet = "Good evening 🌃 ";
   } else if (hour >= 0 && hour <= 5) {
-    greet = "Take some rest 🛏️, ";
+    greet = "Take some rest 🛏️ ";
   }
 
   return greet + name;
