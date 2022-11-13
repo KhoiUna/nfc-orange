e76b146b7fc92f91f5fa2e1a0a03ed867c7f0ad4
KhoiNguyen <thorwaitson@gmail.com>
2022-11-13 15:08:21 -0600

    update ViewLayout.tsx

--- containers/ViewLayout.tsx
+++ containers/ViewLayout.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Link from "next/link";
-import Script from "next/script";
 import { ReactNode } from "react";
 import Analytics from "../components/Analytics";
 import Logo from "../components/Logo";
@@ -19,7 +18,6 @@ const ViewLayout = ({ children, title }: LayoutProps) => {
       <Head>
         <meta charSet="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="NFC ORANGE" />
         <link rel="icon" href="/nfc-orange.svg" />
         <link rel="shortcut icon" href="/nfc-orange.svg" type="image/x-icon" />
