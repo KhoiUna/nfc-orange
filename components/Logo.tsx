2f66bf89f45fe08b7e8edaf50d3ad1603bc5f3c1
KhoiNguyen <thorwaitson@gmail.com>
2022-11-14 22:56:25 -0600

    update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -24,7 +24,7 @@ const Logo = () => (
         filterUnits="userSpaceOnUse"
         colorInterpolationFilters="sRGB"
       >
-        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
         <feColorMatrix
           in="SourceAlpha"
           type="matrix"
