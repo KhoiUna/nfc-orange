2fb43b420257266a2628defe9c1257bf45c521a6
KhoiNguyen <thorwaitson@gmail.com>
2022-11-15 11:30:49 -0600

    update about.tsx page: flyer full page

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,18 +6,17 @@ export default function About() {
   return (
     <Layout title="Contact">
       <div id="parallax" className="text-center p-6 min-h-[80vh]">
-        <div className="bg-slate-50 w-full p-6 mt-[7rem] rounded-lg mx-auto">
-          <h1 className="text-[2rem] font-bold">About us</h1>
+        <div className="mt-[4rem]">
+          <h1
+            className="text-[2.5rem] text-white font-bold m-2"
+            style={{
+              textShadow: "2px 8px 2px rgba(0, 0, 0, 0.3)",
+            }}
+          >
+            About us
+          </h1>
 
-          <div className="m-4">
-            <div className="my-3">
-              <Image
-                className="rounded-lg"
-                src={flyer}
-                alt="NFC Orange flyer"
-              />
-            </div>
-          </div>
+          <Image className="rounded-lg" src={flyer} alt="NFC Orange flyer" />
         </div>
       </div>
     </Layout>
