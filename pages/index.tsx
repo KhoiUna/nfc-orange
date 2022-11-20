6a80776fbf2c3bd2c92c68a2e0c10355e1c6ec0e
KhoiNguyen <thorwaitson@gmail.com>
2022-11-19 19:32:16 -0600

    add intro video to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import Layout from "../containers/Layout";
 
 export default function Index() {
@@ -6,13 +5,23 @@ export default function Index() {
     <Layout title="Home">
       <div id="parallax" className="text-center py-[20vh] min-h-[80vh]">
         <h1
-          className="text-6xl m-8 mt-[7.5rem] text-white font-bold"
+          className="text-6xl m-8 mt-0 text-white font-bold"
           style={{
             textShadow: "2px 8px 2px rgba(0, 0, 0, 0.3)",
           }}
         >
           A tap into your world
         </h1>
+
+        <div className="bg-white p-4 mt-0 rounded-lg w-fit sm:mx-auto m-8 shadow-primary shadow-2xl">
+          <h2 className="text-3xl font-bold pt-2 pb-6">What is NFC Orange?</h2>
+          <video
+            width={800}
+            className="rounded-lg m-auto"
+            src={"/videos/intro-video.mp4"}
+            controls
+          ></video>
+        </div>
       </div>
     </Layout>
   );
