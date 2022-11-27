527639548853cca759b84417f8c7220fc052d2cb
KhoiNguyen <thorwaitson@gmail.com>
2022-11-26 23:50:35 -0600

    web: update home page UI

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -4,7 +4,7 @@ import Layout from "../containers/Layout";
 export default function Index() {
   return (
     <Layout title="Home">
-      <div id="parallax" className="text-center pt-[18vh] pb-4 min-h-[80vh]">
+      <div id="parallax" className="text-center pt-[18vh] min-h-[80vh]">
         <h1
           className="text-6xl m-8 mt-0 text-white font-bold"
           style={{
@@ -23,13 +23,13 @@ export default function Index() {
           ></video>
 
           <Link href={"/shop"}>
-            <button className="text-[1.5rem] font-bold bg-primary w-fit py-2 px-6 text-white rounded-[100px] cursor-pointer mt-4 mx-auto shadow-stone-800 shadow-lg">
+            <button className="text-2xl font-bold bg-primary w-fit py-2 px-6 text-white rounded-[100px] cursor-pointer mt-4 mx-auto shadow-stone-800 shadow-lg">
               Get your card
             </button>
           </Link>
         </div>
 
-        <div className="p-4 rounded-lg mx-8">
+        <div className="p-4 rounded-lg mx-auto px-8 pb-12 max-w-3xl">
           <p
             className="text-lg text-white"
             style={{
@@ -38,15 +38,19 @@ export default function Index() {
           >
             NFC Orange is a card for students, academic people, and job
             searchers that helps them to keep their academic documents like
-            resumes, Academic achievements like certificates at a tap away. We
-            provide a dashboard where you can upload your document, remove and
-            make changes and be on the move without the worry of carrying
-            hardcopies or softcopies or filling up your devic memory with
-            numerous documents making it time-consuming to pull out the required
-            document in time. You can use NFC Orange on any NFC-enabled mobile
-            without installing any app or software. You can use our card to save
-            time to instantly show your resume or any other important document
-            to professors, potential recruiters, etc.
+            resumes, academic achievements like certificates at a tap away.
+            <br />
+            <br />
+            We provide a dashboard where you can upload your document, remove
+            and make changes and be on the move without the worry of carrying
+            hardcopies or filling up your device memory with numerous documents
+            making it time-consuming to pull out the required document in time.
+            <br />
+            <br />
+            You can use NFC Orange on any NFC-enabled smartphones without
+            installing any apps or software. To save time, you can use our card
+            to instantly show your resume or any other important documents to
+            professors, recruiters, etc.
           </p>
         </div>
       </div>
