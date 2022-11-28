c79ab5dd93d11d554c3787bc2982c520de55c3bf
KhoiNguyen <thorwaitson@gmail.com>
2022-11-27 20:33:54 -0600

    web: update view page UI

--- web/pages/view/[cardUuid].tsx
+++ web/pages/view/[cardUuid].tsx
@@ -9,6 +9,7 @@ import { useEffect, useState } from "react";
 import { RenderParameters } from "pdfjs-dist/types/src/display/api";
 import ViewLayout from "../../containers/ViewLayout";
 import { useCookies } from "react-cookie";
+import PDFViewer from "../../components/PDFViewer";
 
 export default function View() {
   const router = useRouter();
@@ -19,59 +20,6 @@ export default function View() {
   const [maxPagesInPDF, setMaxPagesInPDF] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
   const [pdfIsLoading, setPDFIsLoading] = useState(false);
-
-  // Render PDF
-  useEffect(() => {
-    setPDFIsLoading(true);
-
-    if (data?.success.length > 0) {
-      const pdfURL = data.success[0].url;
-
-      GlobalWorkerOptions.workerSrc =
-        "https://cdn.jsdelivr.net/npm/pdfjs-dist@2.16.105/build/pdf.worker.js";
-
-      const loadingTask = getDocument(pdfURL);
-      loadingTask.promise.then(
-        function (pdf) {
-          console.log("PDF loaded");
-
-          // Set pagesInPDF equal to number of pages in PDF
-          setMaxPagesInPDF(pdf.numPages);
-
-          // Fetch the first page
-          pdf.getPage(currentPage).then(function (page) {
-            console.log("PDF page loaded");
-
-            const scale = 2;
-            const viewport = page.getViewport({ scale });
-
-            // Prepare canvas using PDF page dimensions
-            const canvas = document.getElementById(
-              "the-canvas"
-            ) as HTMLCanvasElement;
-            const context = canvas.getContext("2d");
-            canvas.height = viewport.height;
-            canvas.width = viewport.width;
-
-            // Render PDF page into canvas context
-            const renderContext = {
-              canvasContext: context,
-              viewport: viewport,
-            };
-            const renderTask = page.render(renderContext as RenderParameters);
-            renderTask.promise.then(function () {
-              console.log("PDF rendered");
-              setPDFIsLoading(false);
-            });
-          });
-        },
-        function (error) {
-          // PDF loading error
-          console.error("Error rendering PDF");
-        }
-      );
-    }
-  }, [data, currentPage]);
 
   // Update scan history
   const [cookies, setCookie, removeCookie] = useCookies(["viewed"]);
@@ -116,16 +64,34 @@ export default function View() {
 
   if (error)
     return (
-      <Layout title="View">
-        <div className="text-center py-[20vh] min-h-[80vh] m-4">
+      <Layout
+        title={
+          error === "register" ? "View - Please Register" : "View - Invalid"
+        }
+      >
+        <div id="parallax" className="text-center py-[20vh] min-h-[80vh] m-0">
           {error === "invalid" && (
-            <h1 className="text-[3em] font-bold">Invalid card</h1>
+            <h1
+              className="text-white text-[3em] font-bold"
+              style={{
+                textShadow: "2px 8px 2px rgba(0, 0, 0, 0.3)",
+              }}
+            >
+              Invalid card
+            </h1>
           )}
           {error === "register" && (
             <>
-              <h1 className="text-[3em] font-bold">Card is not registered</h1>
+              <h1
+                className="text-[3em] text-white font-bold"
+                style={{
+                  textShadow: "2px 8px 2px rgba(0, 0, 0, 0.3)",
+                }}
+              >
+                Card is not registered
+              </h1>
               <Link href={`/register?c_id=${cardUuid}`}>
-                <h2 className="underline text-primary text-[2rem] mt-3 cursor-pointer">
+                <h2 className="underline text-white text-[2rem] mt-3 cursor-pointer">
                   Go here to register
                 </h2>
               </Link>
@@ -143,21 +109,6 @@ export default function View() {
         </div>
       </Layout>
     );
-
-  // TODO: remove redirecting on Android
-  // if (isAndroid)
-  //   return (
-  //     <Layout title="View">
-  //       <div className="text-center py-[20vh] min-h-[80vh]">
-  //         <meta
-  //           httpEquiv="refresh"
-  //           content={`0; url = https://drive.google.com/viewerng/viewer?embedded=true&url=${encodeURI(
-  //             success[0].url
-  //           )}`}
-  //         />
-  //       </div>
-  //     </Layout>
-  //   );
 
   const handleClick = (action: "back" | "next") => {
     if (currentPage <= 1 && action === "back") return;
@@ -218,11 +169,12 @@ export default function View() {
               </div>
             )}
 
-            <div
+            <PDFViewer pdfURL={data.success[0].url} />
+            {/* <div
               className={`overflow-auto max-w-[380px] max-h-[700px] rounded-lg`}
             >
               <canvas id="the-canvas" className="m-auto w-fit h-fit" />
-            </div>
+            </div> */}
           </div>
         ))}
       </div>
