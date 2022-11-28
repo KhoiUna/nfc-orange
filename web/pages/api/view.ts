c4acdb92e1b10210748c8b3de8a45b5f0355612b
KhoiNguyen <thorwaitson@gmail.com>
2022-11-27 20:34:18 -0600

    web: udpate view.ts api

--- web/pages/api/view.ts
+++ web/pages/api/view.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "../../db/client";
 import { ApiResponse } from "./register";
+import { validate as uuidValidate } from "uuid";
 
 export default async function view(
   req: NextApiRequest,
@@ -13,6 +14,12 @@ export default async function view(
         .json({ success: false, error: "Method not allowed" });
 
     const { c_id } = req.query;
+
+    if (!c_id)
+      return res.status(400).json({ success: false, error: "invalid" });
+
+    if (!uuidValidate(c_id as string))
+      return res.status(400).json({ success: false, error: "invalid" });
 
     // Check if card exists
     const { rows } = await client.query(
