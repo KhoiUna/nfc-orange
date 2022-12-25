45a6082dfabf10cc0a89c3e80923301ee6afaaa3
Khoi T. Nguyen <thorwaitson@gmail.com>
2022-12-25 13:00:16 -0600

    /nfc-reader: update db client.ts pg connection

--- nfc-reader/node-server/src/db/client.ts
+++ nfc-reader/node-server/src/db/client.ts
@@ -1,6 +1,11 @@
+import { config } from "dotenv";
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize(process.env.DATABASE_URI as string);
+config();
+
+export const sequelize = new Sequelize(process.env.DATABASE_URI as string, {
+  dialect: "postgres",
+});
 
 // Test the connection to the database
 sequelize
