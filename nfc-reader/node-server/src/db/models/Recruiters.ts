e1be4323aa3263a7ae0d6d03c08efed7819f15c2
Khoi T. Nguyen <thorwaitson@gmail.com>
2022-12-25 12:44:07 -0600

    /nfc-reader: add Recruiters.ts sequelize model

---
+++ nfc-reader/node-server/src/db/models/Recruiters.ts
@@ -0,0 +1,35 @@
+import Sequelize from "sequelize";
+import { sequelize } from "../client";
+
+const ReaderHistory = sequelize.define("companies", {
+  first_name: {
+    type: Sequelize.STRING(45),
+  },
+  middle_name: {
+    type: Sequelize.STRING(45),
+  },
+  last_name: {
+    type: Sequelize.STRING(45),
+  },
+  email: {
+    type: Sequelize.STRING(255),
+    allowNull: false,
+  },
+  password: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+  created_at: {
+    type: Sequelize.TIME,
+    allowNull: false,
+  },
+  updated_at: {
+    type: Sequelize.TIME,
+  },
+  company_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+});
+
+export default ReaderHistory;
