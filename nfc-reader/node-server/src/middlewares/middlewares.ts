ff421917d90229d4990aa09ece1d827ae539fd74
Khoi T. Nguyen <thorwaitson@gmail.com>
2022-12-26 11:13:49 -0600

    /nfc-reader: update middleware.ts

--- nfc-reader/node-server/src/middlewares/middlewares.ts
+++ nfc-reader/node-server/src/middlewares/middlewares.ts
@@ -7,24 +7,33 @@ export const validateReader = async (
   res: express.Response,
   next: express.NextFunction
 ) => {
-  const readerID = req.body?.reader_id;
+  try {
+    const readerID = req.body?.reader_id;
 
-  if (!readerID || !readerID.trim()) {
-    return res.status(400).send({ success: false, error: "Missing reader ID" });
+    if (!readerID || !readerID.trim()) {
+      return res
+        .status(400)
+        .send({ success: false, error: "Missing reader ID" });
+    }
+
+    // Check if reader in database
+    const reader = await Readers.findOne({
+      where: {
+        serial_number: readerID,
+      },
+    });
+
+    if (!reader) {
+      return res.status(400).send({ success: false, error: "Invalid reader" });
+    }
+
+    next();
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: "Internal server error",
+    });
   }
-
-  // Check if reader in database
-  const reader = await Readers.findOne({
-    where: {
-      serial_number: readerID,
-    },
-  });
-
-  if (!reader) {
-    return res.status(400).send({ success: false, error: "Invalid reader" });
-  }
-
-  next();
 };
 
 export const validateCardSerialNumber = async (
@@ -32,29 +41,36 @@ export const validateCardSerialNumber = async (
   res: express.Response,
   next: express.NextFunction
 ) => {
-  const serialNumber: string = req.body?.serial_number;
+  try {
+    const serialNumber: string = req.body?.serial_number;
 
-  if (!serialNumber || !serialNumber.trim()) {
-    return res
-      .status(400)
-      .send({ success: false, error: "Missing serial number" });
+    if (!serialNumber || !serialNumber.trim()) {
+      return res
+        .status(400)
+        .send({ success: false, error: "Missing serial number" });
+    }
+
+    if (serialNumber.length !== 14) {
+      return res
+        .status(400)
+        .send({ success: false, error: "Invalid serial number" });
+    }
+
+    //  Check if card in database
+    const card = await Cards.findOne({
+      where: {
+        serial_number: serialNumber,
+      },
+    });
+
+    if (!card)
+      return res.status(400).send({ success: false, error: "Invalid card" });
+
+    next();
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: "Internal server error",
+    });
   }
-
-  if (serialNumber.length !== 14) {
-    return res
-      .status(400)
-      .send({ success: false, error: "Invalid serial number" });
-  }
-
-  //  Check if card in database
-  const card = await Cards.findOne({
-    where: {
-      serial_number: serialNumber,
-    },
-  });
-
-  if (!card)
-    return res.status(400).send({ success: false, error: "Invalid card" });
-
-  next();
 };
