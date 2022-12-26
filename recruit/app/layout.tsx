1a97ae6c0708f9c49f2b2a33ea9ca897ecec1d28
Khoi T. Nguyen <thorwaitson@gmail.com>
2022-12-26 14:20:45 -0600

    initialize NextJS app for recruiters /recruit

---
+++ recruit/app/layout.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import "../styles/globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ recruit/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Page() {
+  return (
+    <>
+      <div className="bg-primary p-4">
+        <h1 className="text-white text-xl">Home</h1>
+      </div>
+    </>
+  );
+}
