d2322935a14d745c667be5c7410c38a12e2a96c2
Khoi T. Nguyen <thorwaitson@gmail.com>
2022-12-29 17:07:35 -0600

    /recruit: update api profile.ts

--- recruit/pages/api/profile.ts
+++ recruit/pages/api/profile.ts
@@ -16,19 +16,12 @@ async function profile(req: NextApiRequest, res: NextApiResponse<ApiResponse>) {
         .status(403)
         .json({ success: false, error: "Method not allowed" });
 
-    const { rows: links } = await client.query(
-      "SELECT url FROM links WHERE user_id = (SELECT id FROM users WHERE email = $1);",
-      [req.session.user?.email]
-    );
-
     const { rows } = await client.query(
-      "SELECT first_name, middle_name, last_name FROM users WHERE email = $1;",
-      [req.session.user?.email]
+      "SELECT first_name, middle_name, last_name FROM recruiters WHERE email = $1;",
+      [req.session.user.email]
     );
 
-    return res
-      .status(200)
-      .json({ success: { user: rows[0], links }, error: false });
+    return res.status(200).json({ success: rows[0], error: false });
   } catch (error) {
     console.error(error);
     return res
