8ea45ea4109ec0e59ae6407dfdd1c8bdc51eb06d
Khoi T. Nguyen <thorwaitson@gmail.com>
2023-01-16 17:36:45 -0600

    modified:   nfc-reader/particle-reader/particle-reader.ino

--- nfc-reader/particle-reader/particle-reader.ino
+++ nfc-reader/particle-reader/particle-reader.ino
@@ -1,3 +1,5 @@
+// This #include statement was automatically added by the Particle IDE.
+#include <ArduinoJson.h>
 #include <SPI.h>
 #include <MFRC522.h>
 
@@ -8,34 +10,58 @@
 #define SCK_PIN SCK
 
 
+int led1 = A0; // Success LED
+int led2 = A1; // Failure LED
+
+
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
+
 void setup() {
-	Serial.begin(9600);		// Initialize serial communications with the PC
-	while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
-	SPI.begin();			// Init SPI bus
-	mfrc522.PCD_Init();		// Init MFRC522
-	mfrc522.PCD_DumpVersionToSerial();	// Show details of PCD - MFRC522 Card Reader details
-	Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+	pinMode(led1, OUTPUT);
+    pinMode(led2, OUTPUT);
+    
+	// Subscribe to the integration response event
+    Particle.subscribe("hook-response/reader_scan", successHandler);
+    Particle.subscribe("hook-error/reader_scan", failureHandler);
+    
+	mfrc522.PCD_Init();
+}
+
+
+void successHandler(const char *event, const char *data) {
+  digitalWrite(led1, HIGH);
+  delay(1000);
+  digitalWrite(led1, LOW);
+  delay(1000);
+}
+
+void failureHandler(const char *event, const char *data) {
+  digitalWrite(led2, HIGH);
+  delay(1000);
+  digitalWrite(led2, LOW);
+  delay(1000);
 }
 
 
 void loop() {
 	// Look for new cards
-	if ( ! mfrc522.PICC_IsNewCardPresent()) {
+	if (!mfrc522.PICC_IsNewCardPresent()) {
 		return;
 	}
 
 	// Select one of the cards
-	if ( ! mfrc522.PICC_ReadCardSerial()) {
+	if (!mfrc522.PICC_ReadCardSerial()) {
 		return;
 	}
 
 	
-	String content = "";
+	String serial_number = "";
 	for (byte i = 0; i < mfrc522.uid.size; i++) {
-        content += String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
-        content += String(mfrc522.uid.uidByte[i], HEX);
+        serial_number += String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
+        serial_number += String(mfrc522.uid.uidByte[i], HEX);
     }
-    Particle.publish("rfid_scan", content); // call webhook here
+    
+    Particle.publish("reader_scan", serial_number, PRIVATE); 
+    delay(3000);
 }
