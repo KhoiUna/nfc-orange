5ff5a94ee822eeb0bada5c43ce511bd42e5db43d
Khoi T. Nguyen <thorwaitson@gmail.com>
2023-02-03 14:49:10 -0600

    modified:   web/next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -6,6 +6,18 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+      },
+      {
+        protocol: "https",
+        hostname: "api.dicebear.com",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
