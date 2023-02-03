ea913259fdd829fbb000a90a12d6a9cef7157df8
Khoi T. Nguyen <thorwaitson@gmail.com>
2023-02-03 16:38:02 -0600

    modified:   web/app/profile/page.tsx

--- web/app/profile/page.tsx
+++ web/app/profile/page.tsx
@@ -2,6 +2,7 @@
 
 import { Icon } from "@iconify/react";
 import Image from "next/image";
+import { useState } from "react";
 import useSWR from "swr";
 import TextLoader from "../../components/ui/TextLoader";
 import { swrFetcher } from "../../lib/swrFetcher";
@@ -19,9 +20,11 @@ type Profile = {
 }
 
 export default function Profile() {
-    useAuth({})
+    useAuth({});
 
     const { data: profileResponse, error } = useSWR<Profile, any>("/api/profile", swrFetcher);
+
+    const [isLoading, setIsLoading] = useState(false)
 
     if (error)
         return (
@@ -29,6 +32,8 @@ export default function Profile() {
                 <h1>Failed to load</h1>
             </div>
         );
+
+
 
     if (!profileResponse) return (
         <div className="text-[1.8rem] text-center m-5">
@@ -38,23 +43,31 @@ export default function Profile() {
 
     const { user } = profileResponse.success
 
+    const handleUpload = () => {
+        // TODO
+    }
+
     return (
         <div className="m-auto w-fit my-10">
-            <div>
-                <Image className="m-auto my-8 rounded-[100%]" src={user.avatarURL ||
+            <div className="rounded-lg p-3 m-auto mt-5">
+                <Image className="m-auto rounded-[100%]" src={user?.avatarURL ||
                     `https://api.dicebear.com/5.x/initials/png?seed=${user.first_name} ${user.last_name}`
                 } alt={`${user.first_name}'s profile picture`} width={120} height={120} />
+            </div>
 
+            <button className="text-lg bg-blue-100 rounded-lg mb-5 p-3 flex drop-shadow-lg m-auto text-blue-800 active:drop-shadow-none cursor-pointer">
+                {!isLoading && "Update your avatar"}
+                {isLoading && <TextLoader loadingText="Uploading" />}
                 <Icon className="text-3xl ml-2" icon="ant-design:upload-outlined" />
                 <input
                     required
                     className="absolute left-0 opacity-0 cursor-pointer w-full"
                     type="file"
-                    name="resume_upload"
-                // onChange={handleUpload}
+                    name="avatar_upload"
+                    onChange={handleUpload}
                 // value={path}
                 />
-            </div>
+            </button>
 
             <div className="text-lg">
                 <p><b>First Name:</b> {user.first_name}</p>
