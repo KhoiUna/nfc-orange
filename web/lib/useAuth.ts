c5a52f32e1bee1e90eb1e0876978e11668f12681
Khoi T. Nguyen <thorwaitson@gmail.com>
2023-02-03 14:28:37 -0600

    modified:   web/lib/useAuth.ts

--- web/lib/useAuth.ts
+++ web/lib/useAuth.ts
@@ -19,8 +19,10 @@ export default function useAuth({
   const { data } = useSWR(`/api/user`, swrFetcher);
 
   useEffect(() => {
-    if (!data?.isAuthenticated) router.push("/login");
-    if (data?.isAuthenticated && redirectIfFound) router.push("/dashboard");
+    if (!data) return;
+
+    if (!data.isAuthenticated) router.push("/login");
+    if (data.isAuthenticated && redirectIfFound) router.push("/dashboard");
   }, [data, redirectIfFound, router]);
 
   return { data };
