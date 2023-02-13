1840da04788b6025c95807a17550ab344dbdc9e5
Khoi T. Nguyen <thorwaitson@gmail.com>
2023-02-12 21:39:28 -0600

    modified:   web/pages/dashboard.tsx - add Symplicity PDF URL upload

--- web/pages/dashboard.tsx
+++ web/pages/dashboard.tsx
@@ -9,9 +9,14 @@ import useSWR from "swr";
 import { swrFetcher } from "../lib/swrFetcher";
 import greetUser from "../lib/greetUser";
 import toast, { Toaster } from "react-hot-toast";
+import PDFViewer from "tailwind-pdf-viewer/dist";
+import axios from "axios";
 
 export default function Dashboard() {
   const [isLoading, setIsLoading] = useState(false);
+
+  const [symplicityLink, setSymplicityLink] = useState("")
+  const [symplicityURLValidated, setSymplicityURLValidated] = useState<boolean | null>(null)
 
   const [path, setPath] = useState("");
   const [uploadedURL, setUploadedURL] = useState("");
@@ -19,10 +24,8 @@ export default function Dashboard() {
   const { data, error } = useSWR("/api/profile", swrFetcher);
 
   useEffect(() => {
-    if (data?.success.links[0]?.url) {
-      const pdfURL = data.success.links[0].url;
-      setUploadedURL(pdfURL);
-    }
+    if (data?.success?.pdf_link) setUploadedURL(data.success.pdf_link)
+    if (data?.success?.symplicity_link) setSymplicityLink(data.success.symplicity_link)
   }, [data]);
 
   const handleUpload = async (event: SyntheticEvent) => {
@@ -87,6 +90,115 @@ export default function Dashboard() {
       </AppLayout>
     );
 
+  const handleSubmitSymplicity = async (event: SyntheticEvent) => {
+    try {
+      event.preventDefault()
+      setIsLoading(true);
+
+      const { data } = await axios.post('/api/upload/symplicity', { symplicityLink })
+
+      if (data.error) throw error;
+
+      setIsLoading(false);
+      toast.success("Save successfully!\nYou can tap your card on any NFC-enabled mobile phones to show your resume!")
+    } catch (error: any) {
+      console.error("Error saving Symplicity link")
+      setIsLoading(false)
+      toast.error(error)
+    }
+  };
+
+  const handleChange = async (event: SyntheticEvent) => {
+    try {
+      const target = event.target as HTMLInputElement
+      const url = new URL(target.value)
+      const params = url.searchParams
+
+      const SYMPLICITY_VALIDATE_URL = '/symp.csm.usprod/una/files'
+      const targetPathname = url.pathname.slice(0, 26)
+
+      if (SYMPLICITY_VALIDATE_URL !== targetPathname || !url.search) throw new Error("Invalid Symplicity URL");
+
+      const SYMPLICITY_VALIDATE_KEYS = [
+        'X-Amz-Content-Sha256', 'X-Amz-Algorithm', 'X-Amz-Credential', 'X-Amz-Date', 'X-Amz-SignedHeaders', 'X-Amz-Expires', 'X-Amz-Signature'
+      ]
+      for (const [key, value] of params) {
+        if (!SYMPLICITY_VALIDATE_KEYS.includes(key) || !value) throw new Error("Invalid Symplicity URL");
+      }
+
+      setSymplicityLink(target.value)
+      setSymplicityURLValidated(true)
+    } catch (error: any) {
+      console.error(error.message);
+      setSymplicityURLValidated(false)
+      toast.error(error.message)
+    }
+  }
+
+  if (symplicityLink) return (
+    <AppLayout title="Dashboard">
+      <Toaster
+        toastOptions={{
+          success: {
+            style: {
+              background: "green",
+              fontWeight: "bold",
+              fontSize: "large",
+              color: "white",
+            },
+          },
+          error: {
+            style: {
+              background: "red",
+              fontWeight: "bold",
+              fontSize: "large",
+              color: "white",
+            },
+          },
+        }}
+      />
+
+      <h2 className="text-xl mx-5 my-7">
+        {greetUser(data.success.user.first_name)}
+      </h2>
+
+      <div className="text-center">
+        <form onSubmit={handleSubmitSymplicity}>
+          <div className="m-5">
+            <p className="text-left mb-2 font-bold">Paste Symplicity Resume URL</p>
+            <input
+              required
+              placeholder="Paste URL here"
+              className={`w-full sm:w-[500px] border-2 border-slate-200 p-2 rounded-lg focus:outline-none ${symplicityURLValidated === true && 'border-green-500'} ${symplicityURLValidated === false && 'border-red-500'}`}
+              type="text"
+              onChange={handleChange}
+              value={symplicityLink}
+            />
+          </div>
+          <button
+            className="text-md bg-blue-100 rounded-lg p-2 drop-shadow-lg m-auto text-blue-800 active:drop-shadow-none"
+          >
+            {!isLoading && "Save"}
+            {isLoading && <TextLoader loadingText="Saving" />}
+          </button>
+        </form>
+
+        <p className="mt-3 text-sm text-center italic">*Only approved PDFs from<br />Symplicity are allowed</p>
+      </div>
+
+      {!symplicityLink && (
+        <p className="text-[1.3rem] p-2 font-bold my-1 text-center">
+          No PDF uploaded
+        </p>
+      )}
+
+
+      <div className="mt-9">
+        <PDFViewer pdfURL={symplicityLink} />
+      </div>
+    </AppLayout>
+  )
+
   return (
     <AppLayout title="Dashboard">
       <Toaster
@@ -114,7 +226,33 @@ export default function Dashboard() {
         {greetUser(data.success.user.first_name)}
       </h2>
 
-      <button className="text-xl bg-blue-100 rounded-lg p-3 flex drop-shadow-lg m-auto my-5 text-blue-800 active:drop-shadow-none">
+      {/* Upload Symplicity PDF URL */}
+      <div className="text-center">
+        <form>
+          <div className="m-5">
+            <p className="text-left mb-2 font-bold">Paste Symplicity Resume URL</p>
+            <input
+              required
+              placeholder="Paste URL here"
+              className={`w-full sm:w-[500px] border-2 border-slate-200 p-2 rounded-lg focus:outline-none ${symplicityURLValidated === true && 'border-green-500'} ${symplicityURLValidated === false && 'border-red-500'}`}
+              type="text"
+              onChange={handleChange}
+              value={symplicityLink}
+            />
+          </div>
+          <button
+            className="text-md bg-blue-100 rounded-lg p-2 drop-shadow-lg m-auto text-blue-800 active:drop-shadow-none"
+          >
+            {!isLoading && "Save"}
+            {isLoading && <TextLoader loadingText="Saving" />}
+          </button>
+        </form>
+
+        <p className="mt-3 text-sm text-center italic">*Only approved PDFs from<br />Symplicity are allowed</p>
+      </div>
+
+      {/* Upload custom PDF */}
+      {/* <button className="text-xl bg-blue-100 rounded-lg p-3 flex drop-shadow-lg m-auto my-5 text-blue-800 active:drop-shadow-none">
         {!isLoading && "Upload your PDF"}
         {isLoading && <TextLoader loadingText="Uploading" />}
         <Icon className="text-3xl ml-2" icon="ant-design:upload-outlined" />
@@ -127,7 +265,7 @@ export default function Dashboard() {
           value={path}
         />
       </button>
-      <p className="text-sm text-center italic">*Only PDFs are allowed</p>
+      <p className="text-sm text-center italic">*Only PDFs are allowed</p> */}
 
       {!uploadedURL && (
         <p className="text-[1.3rem] p-2 font-bold my-1 text-center">
