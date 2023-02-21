249749d8a3b07851c955f66cc15566b953d536a5
Khoi T. Nguyen <thorwaitson@gmail.com>
2023-02-21 14:52:51 -0600

    modified:   web/next.config.js - enable CORS

--- web/next.config.js
+++ web/next.config.js
@@ -18,6 +18,30 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "nfcorange-download.khoiuna.info",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-SHA-256, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
