3e9144a5c0210c0ff294dc0d281b5211b31c0264
Khoi T. Nguyen <thorwaitson@gmail.com>
2023-02-22 10:18:46 -0600

    modified:   web/pages/api/upload/symplicity.ts

--- web/pages/api/upload/symplicity.ts
+++ web/pages/api/upload/symplicity.ts
@@ -20,63 +20,24 @@ async function upload(req: NextApiRequest, res: NextApiResponse<ApiResponse>) {
 
     const { symplicityLink } = req.body;
 
-    // const { data } = await axios.post(
-    //   `${process.env.NODE_DOWNLOAD_API}/api/download`,
-    //   {
-    //     user_email: req.session.user.email,
-    //     download_url: symplicityLink,
-    //   },
-    //   {
-    //     timeout: 3000,
-    //   }
-    // );
-    // if (data.error) throw new Error("POST error to node-download-server");
+    // Save link to database
+    const response = await client.query(
+      "INSERT INTO symplicity_resume_links(user_id, url, updated_at) VALUES ((SELECT id FROM users WHERE email = $1), $2, $3) ON CONFLICT (user_id) DO UPDATE SET url = $2, updated_at = $3;",
+      [req.session.user?.email, symplicityLink, new Date()]
+    );
+    if (!response) throw "Error saving Symplicity link";
 
-    // const pdfURL = data.success;
-    // console.log(JSON.stringify(data));
-
-    // // Save link to database
-    // const response = await client.query(
-    //   "INSERT INTO symplicity_resume_links(user_id, url, updated_at) VALUES ((SELECT id FROM users WHERE email = $1), $2, $3) ON CONFLICT (user_id) DO UPDATE SET url = $2, updated_at = $3;",
-    //   [req.session.user?.email, pdfURL, new Date()]
-    // );
-    // if (!response) throw "Error saving Symplicity link";
-
-    // return res.status(200).json({ success: true, error: false });
-
-    //
-    //
-    //
-    axios
-      .post(`${process.env.NODE_DOWNLOAD_API}/api/download`, {
+    // Ping node-download-server to update download and replace Symplicity URL with Firebase URL in db
+    const { data } = await axios.post(
+      `${process.env.NODE_DOWNLOAD_API}/api/download`,
+      {
+        user_email: req.session.user.email,
         download_url: symplicityLink,
-      })
-      .then(({ data }) => {
-        console.log(JSON.stringify(data));
-        return res.status(200).json({ success: true, error: false });
-      })
-      .catch((error) => {
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        } else if (error.request) {
-          // The request was made but no response was received
-          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-          // http.ClientRequest in node.js
-          console.log(error.request);
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.log("Error", error.message);
-        }
-        console.log(error.config);
+      }
+    );
+    if (data.error) throw new Error("POST error to node-download-server");
 
-        return res
-          .status(500)
-          .json({ success: false, error: "Internal server error" });
-      });
+    return res.status(200).json({ success: true, error: false });
   } catch (error) {
     console.error(error);
     return res
