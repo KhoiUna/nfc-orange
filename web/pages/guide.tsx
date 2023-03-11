8c4980b3f1b7dee646cbeced7db5cbe776b009ff
Khoi T. Nguyen <thorwaitson@gmail.com>
2023-03-10 23:44:01 -0600

    modified:   web/pages/guide.tsx

--- web/pages/guide.tsx
+++ web/pages/guide.tsx
@@ -1,12 +1,11 @@
 import Layout from "../containers/Layout";
 import PDFViewer from "tailwind-pdf-viewer/dist";
-import "tailwind-pdf-viewer/dist/style.css";
 
 export default function Guide() {
     return (
         <Layout title="Home">
             <div id="parallax" className="flex justify-center items-center text-center">
-                <div className="w-full mt-[5rem]">
+                <div className="w-full pt-[5rem]">
                     <PDFViewer pdfURL={"/nfcorange-guide-flyer.pdf"} />
                 </div>
             </div>
