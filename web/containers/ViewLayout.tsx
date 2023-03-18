dde8ba2c2f9912f46ca982d6118e68920376da31
Khoi T. Nguyen <thorwaitson@gmail.com>
2023-03-18 16:00:07 -0500

    modified:   web/containers/ViewLayout.tsx

--- web/containers/ViewLayout.tsx
+++ web/containers/ViewLayout.tsx
@@ -13,33 +13,35 @@ interface LayoutProps {
 const ViewLayout = ({ children, title }: LayoutProps) => {
   const pageTitle = `NFC ORANGE | ${title}`;
 
-  return <>
-    <Head>
-      <meta charSet="UTF-8" />
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
-      <meta name="description" content="NFC ORANGE" />
-      <link rel="icon" href="/nfc-orange.svg" />
-      <link rel="shortcut icon" href="/nfc-orange.svg" type="image/x-icon" />
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
+        <meta name="description" content="NFC ORANGE" />
+        <link rel="icon" href="/nfc-orange.svg" />
+        <link rel="shortcut icon" href="/nfc-orange.svg" type="image/x-icon" />
 
-      <title>{pageTitle}</title>
+        <title>{pageTitle}</title>
 
-      <Analytics />
-    </Head>
+        <Analytics />
+      </Head>
 
 
-    <div className="sm:w-screen w-fit">
-      <header className={`flex justify-center p-3 ${HEADER_BAR_BG_COLOR}`}>
-        <Link href="/" legacyBehavior>
-          <div className="w-[10rem] pt-2 h-fit cursor-pointer">
-            <Logo />
-          </div>
-        </Link>
-      </header>
+      <div className="sm:w-screen w-fit">
+        <header className={`flex justify-center p-3 ${HEADER_BAR_BG_COLOR}`}>
+          <Link href="/" legacyBehavior>
+            <div className="w-[10rem] pt-2 h-fit cursor-pointer">
+              <Logo />
+            </div>
+          </Link>
+        </header>
 
-      <main className="w-fit m-auto h-[200vh] bg-stone-400">{children}</main>
-    </div>
-  </>;
+        <main className="w-fit m-auto h-[200vh] bg-stone-400">{children}</main>
+      </div>
+    </>
+  )
 };
 
 export default ViewLayout;
