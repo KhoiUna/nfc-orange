d529b9086e7309e07d35e27a5996d6ad070d3375
Khoi T. Nguyen <thorwaitson@gmail.com>
2023-03-19 01:08:31 -0500

    modified:   web/app/profile/layout.tsx

--- web/app/profile/layout.tsx
+++ web/app/profile/layout.tsx
@@ -1,42 +1,15 @@
 'use client'
 
-import Analytics from "@/components/Analytics";
-import AppHeaderBar from "../../components/ui/AppHeaderBar";
+import AppHeaderBar from "@/components/ui/AppHeaderBar";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
-            <head>
-                <meta name="description" content="NFC ORANGE" />
-                <link rel="icon" href="/nfc-orange.svg" />
-                <link rel="shortcut icon" href="/nfc-orange.svg" type="image/x-icon" />
-
-                <title>NFC ORANGE | Profile</title>
-
-                <Analytics />
-            </head>
-
+        <>
             <AppHeaderBar
                 title={'Profile'}
-                navLinks={[
-                    {
-                        href: "/dashboard",
-                        text: "Dashboard",
-                    },
-                    {
-                        href: "/profile",
-                        text: "Profile",
-                    },
-                    {
-                        href: "/api/logout",
-                        text: "Logout",
-                    },
-                ]}
             />
 
             <main>{children}</main>
-        </html>
+        </>
     );
 };
-
-export default Layout;
