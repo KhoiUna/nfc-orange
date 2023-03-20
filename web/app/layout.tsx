5d9909b9b499278d7a8b5a0fc62ee4565fd014a3
Khoi T. Nguyen <thorwaitson@gmail.com>
2023-03-19 23:23:14 -0500

    modified:   web/app/layout.tsx

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({ children }: {
     children: React.ReactNode;
 }) {
     return (
-        <html>
+        <html lang='en'>
             <body>
                 <main>{children}</main>
             </body>
