151d635f566cf51bb813ceef3fa70f2f26a3e993
Khoi T. Nguyen <thorwaitson@gmail.com>
2023-03-20 13:30:33 -0500

    new file:   web/types/types.ts

---
+++ web/types/types.ts
@@ -0,0 +1,10 @@
+export type Student = {
+  student_id: number;
+  first_name: string;
+  middle_name?: string;
+  last_name: string;
+  avatar_url?: string;
+  major: string;
+  university_name: string;
+  uuid: string;
+};
