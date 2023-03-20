e0eb4d3a3b21f5577bfe2a6337de4e1d105e8cdb
Khoi T. Nguyen <thorwaitson@gmail.com>
2023-03-20 18:01:04 -0500

    modified:   web/app/peers/page.tsx

--- web/app/peers/page.tsx
+++ web/app/peers/page.tsx
@@ -7,19 +7,19 @@ import useSWR from "swr";
 import { Student } from "../../types/types";
 import PeerCard from "./components/PeerCard";
 import { register, SwiperContainer, SwiperSlide } from 'swiper/element/bundle';
-import { DOMAttributes, Fragment } from "react";
+import { DOMAttributes } from "react";
 import { isMobile } from "react-device-detect";
 register();
 
-type SwiperAttributes<K extends string> = { [key in K]: string };
+type SwiperAttributes<K extends string> = { [key in K]: any };
 type CustomEvents<K extends string> = { [key in K]: (event: CustomEvent) => void };
 type CustomElement<T, K extends string> = Partial<T & DOMAttributes<T> & SwiperAttributes<K> & { children: any } & CustomEvents<`on${K}`>>;
 
 declare global {
     namespace JSX {
         interface IntrinsicElements {
-            ['swiper-container']: CustomElement<typeof SwiperContainer, 'cssMode' | 'speed' | 'loop'>;
-            ['swiper-slide']: CustomElement<typeof SwiperSlide, ''>;
+            ['swiper-container']: CustomElement<typeof SwiperContainer, 'slides-per-view' | 'navigation' | 'grab-cursor'>;
+            ['swiper-slide']: CustomElement<typeof SwiperSlide, 'key'>;
         }
     }
 }
@@ -42,7 +42,7 @@ export default function Peers() {
 
     if (!data) return (
         <div className="h-[85vh] mt-4">
-            <h1 className="text-center mb-8 mx-3 sm:text-5xl text-4xl font-semibold">Your Peers Network</h1>
+            <h1 id="peers" className="text-center mb-8 mx-3 sm:text-5xl text-4xl font-semibold">Your Peers Network</h1>
 
             <div className="text-[1.8rem] text-center m-5">
                 <TextLoader loadingText="Loading" />
@@ -53,27 +53,22 @@ export default function Peers() {
     const { students } = data.success
 
     return (
-        <div className="h-[85vh] mt-4">
+        <div className="h-[85vh] mt-7">
             <h1 id="peers" className="text-center mb-8 mx-3 sm:text-5xl text-4xl font-semibold">Your Peers Network</h1>
 
-            <div>
-                {/* TODO: separate majors */}
-                <p className="mx-3 mt-3 mb-1 font-semibold">Computer Science</p>
-
-                <swiper-container slides-per-view={isMobile ? 1 : 3} speed="500" loop="true" css-mode={true}>
-                    {
-                        students.map((student: Student, index: number) => (
-                            <Fragment key={index} >
-                                <swiper-slide>
-                                    <PeerCard student={student} />
-                                </swiper-slide>
-                            </Fragment>
-                        ))
-                    }
-                </swiper-container>
-            </div>
-
-
-        </div >
+            <swiper-container
+                slides-per-view={isMobile ? 'auto' : 3}
+                navigation="true"
+                grab-cursor="true"
+            >
+                {
+                    students.map((student: Student, index: number) => (
+                        <swiper-slide key={index}>
+                            <PeerCard student={student} />
+                        </swiper-slide>
+                    ))
+                }
+            </swiper-container>
+        </div>
     )
 }
