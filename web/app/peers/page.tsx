5bb1514d2f0a4ffe7e8c3e336202f2c5e41e87a5
Khoi T. Nguyen <thorwaitson@gmail.com>
2023-03-20 14:20:58 -0500

    modified:   web/app/peers/page.tsx

--- web/app/peers/page.tsx
+++ web/app/peers/page.tsx
@@ -1,91 +1,79 @@
 'use client'
 
+import TextLoader from "@/components/ui/TextLoader";
+import { swrFetcher } from "@/lib/swrFetcher";
 import useAuth from "@/lib/useAuth";
-import { Icon } from "@iconify/react";
-import { Fragment } from "react";
-import Marquee from "react-fast-marquee";
+import useSWR from "swr";
+import { Student } from "../../types/types";
 import PeerCard from "./components/PeerCard";
+import { register, SwiperContainer, SwiperSlide } from 'swiper/element/bundle';
+import { DOMAttributes, Fragment } from "react";
+import { isMobile } from "react-device-detect";
+register();
 
-const DIV_IN_MARQUEE_QUANTITY = 7
-const DIV_IN_MARQUEE_MAP = new Array(DIV_IN_MARQUEE_QUANTITY).fill(null)
-const DivInMarquee = () => (
-    <div className="shadow-lg rounded-lg w-[15rem] h-[100px] mx-3 bg-slate-50 flex items-center">
-        <div className="p-2 border-r-2 border-slate-300 h-fit">
-            <Icon className="text-[2rem] text-slate-300" icon="carbon:user-avatar-filled" />
-        </div>
+type SwiperAttributes<K extends string> = { [key in K]: string };
+type CustomEvents<K extends string> = { [key in K]: (event: CustomEvent) => void };
+type CustomElement<T, K extends string> = Partial<T & DOMAttributes<T> & SwiperAttributes<K> & { children: any } & CustomEvents<`on${K}`>>;
 
-        <div className="w-full p-2 ml-3">
-            <div className="w-full h-[7px] bg-slate-300 rounded-md mt-2" />
-            <div className="w-full h-[7px] bg-slate-300 rounded-md mt-1" />
-            <div className="w-full h-[7px] bg-slate-300 rounded-md mt-3" />
-            <div className="w-[2rem] h-[7px] bg-slate-300 rounded-md mt-3" />
-            <div className="w-[2rem] h-[7px] bg-slate-300 rounded-md mt-1" />
-        </div>
-    </div >
-)
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            ['swiper-container']: CustomElement<typeof SwiperContainer, 'cssMode' | 'speed' | 'loop'>;
+            ['swiper-slide']: CustomElement<typeof SwiperSlide, ''>;
+        }
+    }
+}
 
 export default function Peers() {
-    useAuth({});
+    useAuth({})
+
+    const { data, error } = useSWR<{
+        success: {
+            students: Student[]
+        }
+    }, any>("/api/peers", swrFetcher);
+
+    if (error) return (
+        <div className="text-[1.8rem] text-center m-5">
+            <h1 className="text-center mb-8 mx-3 sm:text-5xl text-4xl font-semibold">Your Peers Network</h1>
+            <h1>Failed to load</h1>
+        </div>
+    );
+
+    if (!data) return (
+        <div className="h-[85vh] mt-4">
+            <h1 className="text-center mb-8 mx-3 sm:text-5xl text-4xl font-semibold">Your Peers Network</h1>
+
+            <div className="text-[1.8rem] text-center m-5">
+                <TextLoader loadingText="Loading" />
+            </div>
+        </div>
+    )
+
+    const { students } = data.success
 
     return (
-        <>
-            <div className="bg-slate-100 pb-3 max-h-screen overflow-hidden">
-                <div className="pt-8">
-                    {DIV_IN_MARQUEE_MAP.map((_, index) => (
-                        <Fragment key={index}>
-                            <Marquee className="mb-8">
-                                {DIV_IN_MARQUEE_MAP.map((_, index) => (
-                                    <DivInMarquee key={index} />
-                                ))}
-                            </Marquee>
-                            <Marquee className="my-8" direction="right">
-                                {DIV_IN_MARQUEE_MAP.map((_, index) => (
-                                    <DivInMarquee key={index} />
-                                ))}
-                            </Marquee>
-                        </Fragment>
-                    ))}
-                </div>
+        <div className="h-[85vh] mt-4">
+            <h1 id="peers" className="text-center mb-8 mx-3 sm:text-5xl text-4xl font-semibold">Your Peers Network</h1>
 
+            <div>
+                {/* TODO: separate majors */}
+                <p className="mx-3 mt-3 mb-1 font-semibold">Computer Science</p>
 
-                <div className="absolute right-0 left-0 text-center top-[30%] shadow-primary z-[5]">
-                    <h1 className="sm:text-5xl text-4xl font-semibold"
-                        style={{
-                            textShadow: '0px 10px 15px #DC7700'
-                        }}
-                    >Welcome to</h1>
-                    <h1 className="sm:text-5xl text-4xl mt-1 font-semibold"
-                        style={{
-                            textShadow: '0px 10px 15px #DC7700'
-                        }}
-                    >PEERS</h1>
-
-                    <h2 className="text-xl sm:text-2xl mx-3 mt-4"
-                        style={{
-                            textShadow: '0px 10px 15px #DC7700'
-                        }}
-                    >Show your professions with the world</h2>
-
-                    <div className="m-auto w-fit mt-6">
-                        <Icon className="text-[3rem]" icon="pajamas:scroll-down" />
-                    </div>
-                </div>
+                <swiper-container slides-per-view={isMobile ? 1 : 3} speed="500" loop="true" css-mode={true}>
+                    {
+                        students.map((student: Student, index: number) => (
+                            <Fragment key={index} >
+                                <swiper-slide>
+                                    <PeerCard student={student} />
+                                </swiper-slide>
+                            </Fragment>
+                        ))
+                    }
+                </swiper-container>
             </div>
 
-            <div className="h-[85vh] mt-4">
-                <h1 className="text-center mb-8 mx-3 sm:text-5xl text-4xl font-semibold">Your Peers Network</h1>
 
-                {/* TODO: map fetched students here */}
-                <div>
-                    <p className="mx-3 mt-3 mb-1 font-semibold">Computer Science</p>
-                    <PeerCard student={{
-                        first_name: 'John',
-                        last_name: 'Doe',
-                        university_name: 'UNA',
-                        major: 'Computer Science'
-                    }} />
-                </div>
-            </div>
-        </>
+        </div >
     )
 }
