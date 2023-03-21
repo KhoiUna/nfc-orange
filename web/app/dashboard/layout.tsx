3c919fa0c3f3f3780b8582174cdcc38b2747af1b
Khoi T. Nguyen <thorwaitson@gmail.com>
2023-03-20 21:15:30 -0500

    new file:   web/app/dashboard/layout.tsx

---
+++ web/app/dashboard/layout.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import AppHeaderBar from "@/components/ui/AppHeaderBar";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            <AppHeaderBar
+                title={'Dashboard'}
+            />
+
+            <main>{children}</main>
+        </>
+    );
+};
