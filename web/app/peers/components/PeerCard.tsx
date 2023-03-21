706d450631d83f00fb04b8b30c968faf9a45181c
Khoi T. Nguyen <thorwaitson@gmail.com>
2023-03-20 21:01:01 -0500

    modified:   web/app/peers/components/PeerCard.tsx

--- web/app/peers/components/PeerCard.tsx
+++ web/app/peers/components/PeerCard.tsx
@@ -7,9 +7,10 @@ interface Props {
 
 export default function PeerCard({ student }: Props) {
     return (
-        <div className="bg-slate-50 rounded-lg my-4 mx-auto p-4 flex items-center hover:drop-shadow-lg w-[350px] sm:w-[400px] h-[150px]">
+        <div className="bg-slate-50 rounded-lg my-4 mx-4 px-4 flex items-center hover:drop-shadow-lg w-[350px] sm:w-[400px] h-[180px]">
             <div className="rounded-lg border-r-2 border-slate-300 pr-8">
                 <Image
+                    loading="lazy"
                     className="m-auto w-[60px] h-[60px] rounded-[100%] border-2 border-primary object-scale-down"
                     src={student.avatar_url
                         || `https://api.dicebear.com/5.x/initials/png?seed=${student.first_name} ${student.last_name}`
@@ -21,7 +22,7 @@ export default function PeerCard({ student }: Props) {
 
                 <p><b>Major:</b> {student.major}</p>
 
-                <p className="py-2">{student.university_name}</p>
+                <p className="pt-2">{student.university_name}</p>
             </div>
         </div>
     )
