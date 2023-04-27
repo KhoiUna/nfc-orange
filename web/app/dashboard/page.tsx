e153c35517ee440bd94b226ebf808b3f9c1f6de8
KhoiUna <thorwaitson@gmail.com>
2023-04-27 18:09:17 -0500

    bring back upload custom PDFs

--- web/app/dashboard/page.tsx
+++ web/app/dashboard/page.tsx
@@ -13,14 +13,10 @@ import { swrFetcher } from "@/lib/swrFetcher";
 import TextLoader from "@/components/ui/TextLoader";
 import greetUser from "@/lib/greetUser";
 import app from "@/lib/firebase";
+import { Icon } from "@iconify/react";
 
 export default function Dashboard() {
     const [isLoading, setIsLoading] = useState(false);
-
-    const [symplicityLink, setSymplicityLink] = useState("")
-    const [previewSymplicityLink, setPreviewSymplicityLink] = useState("")
-    const [symplicityURLValidated, setSymplicityURLValidated] = useState<boolean | null>(null)
-
     const [path, setPath] = useState("");
     const [uploadedURL, setUploadedURL] = useState("");
 
@@ -28,10 +24,8 @@ export default function Dashboard() {
 
     useEffect(() => {
         if (data?.success?.pdf_link) setUploadedURL(data.success.pdf_link)
-        if (data?.success?.symplicity_link) setSymplicityLink(data.success.symplicity_link)
     }, [data]);
 
-    // NOTE: The function below is for uploading custom PDF
     const handleUpload = async (event: SyntheticEvent) => {
         try {
             event.preventDefault();
@@ -90,120 +84,6 @@ export default function Dashboard() {
             </div>
         );
 
-    const handleSubmitSymplicity = async (event: SyntheticEvent) => {
-        try {
-            event.preventDefault()
-            setIsLoading(true);
-
-            const { data } = await axios.post('/api/upload/symplicity', { symplicityLink: previewSymplicityLink })
-
-            if (data.error) throw error;
-
-            setIsLoading(false);
-            toast.success("Save successfully!\nYou can tap your card on any NFC-enabled mobile phones to show your resume!")
-        } catch (error: any) {
-            console.error("Error saving Symplicity link")
-            setIsLoading(false)
-            toast.error(error)
-        }
-    };
-
-    const handleChange = async (event: SyntheticEvent) => {
-        try {
-            const target = event.target as HTMLInputElement
-            const url = new URL(target.value)
-            const params = url.searchParams
-
-            const SYMPLICITY_VALIDATE_URL = '/symp.csm.usprod/una/files'
-            const targetPathname = url.pathname.slice(0, 26)
-
-            if (SYMPLICITY_VALIDATE_URL !== targetPathname || !url.search) throw new Error("Invalid Symplicity URL");
-
-            const SYMPLICITY_VALIDATE_KEYS = [
-                'X-Amz-Content-Sha256', 'X-Amz-Algorithm', 'X-Amz-Credential', 'X-Amz-Date', 'X-Amz-SignedHeaders', 'X-Amz-Expires', 'X-Amz-Signature'
-            ]
-            for (const [key, value] of params) {
-                if (!SYMPLICITY_VALIDATE_KEYS.includes(key) || !value) throw new Error("Invalid Symplicity URL");
-            }
-
-            setPreviewSymplicityLink(target.value)
-            setSymplicityURLValidated(true)
-        } catch (error: any) {
-            console.error(error.message);
-            setPreviewSymplicityLink('')
-            setSymplicityURLValidated(false)
-            toast.error(error.message)
-        }
-    }
-
-    if (symplicityLink) return (
-        <>
-            <Toaster
-                toastOptions={{
-                    success: {
-                        style: {
-                            background: "green",
-                            fontWeight: "bold",
-                            fontSize: "large",
-                            color: "white",
-                        },
-                    },
-                    error: {
-                        style: {
-                            background: "red",
-                            fontWeight: "bold",
-                            fontSize: "large",
-                            color: "white",
-                        },
-                    },
-                }}
-            />
-
-            <h2 className="text-xl mx-5 my-7">
-                {greetUser(data.success.user.first_name)}
-            </h2>
-
-            <ProfilePictureUpload user={data.success.user} />
-
-            <div>
-                <form onSubmit={handleSubmitSymplicity}>
-                    <div className="m-5">
-                        <p className="mt-8 font-bold">Paste Symplicity Resume URL</p>
-                        <Link href={'/guide'} target='_blank' className="block w-fit"><p className="text-sm underline font-semibold italic text-purple-800">How do I do this?</p></Link>
-                        <p className="mt-1 mb-2 text-sm italic">Only approved PDFs from Symplicity are allowed</p>
-
-                        <input
-                            required
-                            placeholder="Paste here"
-                            className={`w-full border-2 border-slate-200 p-2 rounded-lg focus:outline-none ${symplicityURLValidated === true && 'border-green-500'} ${symplicityURLValidated === false && 'border-red-500'}`}
-                            type="text"
-                            onChange={handleChange}
-                            value={previewSymplicityLink}
-                        />
-                    </div>
-
-                    <div className="text-center">
-                        <button
-                            className="text-md bg-blue-100 rounded-lg p-2 drop-shadow-lg m-auto text-blue-800 active:drop-shadow-none"
-                        >
-                            {!isLoading && "Save"}
-                            {isLoading && <TextLoader loadingText="Saving" />}
-                        </button>
-                    </div>
-                </form>
-            </div>
-
-            <div className="mt-6">
-                {!previewSymplicityLink && <p className="mx-5 font-bold mb-2">Your current resume:</p>}
-                {previewSymplicityLink && <p className="mx-5 font-bold mb-2">Resume preview:</p>}
-
-                <div className="mx-5">
-                    <PDFViewer pdfURL={previewSymplicityLink || symplicityLink} />
-                </div>
-            </div>
-        </>
-    )
-
     return (
         <>
             <Toaster
@@ -232,80 +112,36 @@ export default function Dashboard() {
             </h2>
 
             <ProfilePictureUpload user={data.success.user} />
-
-            {/* Upload Symplicity PDF URL */}
-            <div>
-                <form onSubmit={handleSubmitSymplicity}>
-                    <div className="m-5">
-                        <p className="mt-8 font-bold">Paste Symplicity Resume URL</p>
-                        <Link href={'/guide'} target='_blank'><p className="text-sm underline font-semibold italic text-purple-800">How do I do this?</p></Link>
-                        <p className="mt-1 mb-2 text-sm italic">Only approved PDFs from Symplicity are allowed</p>
-
-                        <input
-                            required
-                            placeholder="Paste here"
-                            className={`w-full border-2 border-slate-200 p-2 rounded-lg focus:outline-none ${symplicityURLValidated === true && 'border-green-500'} ${symplicityURLValidated === false && 'border-red-500'}`}
-                            type="text"
-                            onChange={handleChange}
-                            value={previewSymplicityLink}
-                        />
-                    </div>
-
-                    <div className="text-center">
-                        <button
-                            className="text-md bg-blue-100 rounded-lg p-2 drop-shadow-lg m-auto text-blue-800 active:drop-shadow-none"
-                        >
-                            {!isLoading && "Save"}
-                            {isLoading && <TextLoader loadingText="Saving" />}
-                        </button>
-                    </div>
-                </form>
-            </div>
+            <hr className="w-[95%] m-auto" />
 
             {/* Upload custom PDF */}
-            {/* <button className="text-xl bg-blue-100 rounded-lg p-3 flex drop-shadow-lg m-auto my-5 text-blue-800 active:drop-shadow-none">
-        {!isLoading && "Upload your PDF"}
-        {isLoading && <TextLoader loadingText="Uploading" />}
-        <Icon className="text-3xl ml-2" icon="ant-design:upload-outlined" />
-        <input
-          required
-          className="absolute left-0 opacity-0 cursor-pointer w-full"
-          type="file"
-          name="resume_upload"
-          onChange={handleUpload}
-          value={path}
-        />
-      </button>
-      <p className="text-sm text-center italic">*Only PDFs are allowed</p> */}
+            <button className="text-md bg-blue-100 rounded-lg p-3 flex drop-shadow-lg m-auto my-5 text-blue-800 active:drop-shadow-none">
+                {!isLoading && "Upload your PDF"}
+                {isLoading && <TextLoader loadingText="Uploading" />}
+                <Icon className="text-2xl ml-2" icon="ant-design:upload-outlined" />
+                <input
+                    required
+                    className="absolute left-0 opacity-0 cursor-pointer w-full"
+                    type="file"
+                    name="resume_upload"
+                    onChange={handleUpload}
+                    value={path}
+                />
+            </button>
+            <p className="text-sm text-center italic">*Only PDFs are allowed</p>
 
-            {!uploadedURL && !symplicityLink && (
+            {!uploadedURL && (
                 <p className="text-lg p-2 font-bold mt-3 text-center">
                     No PDF saved
                 </p>
             )}
 
-            {(previewSymplicityLink || symplicityLink) && (
-                <div className="mt-6">
-                    {(uploadedURL && !previewSymplicityLink) && <p className="mx-5 font-bold mb-2">Your current resume:</p>}
-                    {(!uploadedURL && previewSymplicityLink) && <p className="mx-5 font-bold mb-2">Resume preview:</p>}
-
-                    <div className="mx-5">
-                        <PDFViewer pdfURL={previewSymplicityLink || symplicityLink} />
-                    </div>
-                </div>
-            )}
-
-            {/* {uploadedURL && (
-        <div className="mt-9">
-          <object
-            className="w-full h-[70vh] m-auto"
-            type="application/pdf"
-            data={`https://drive.google.com/viewerng/viewer?embedded=true&url=${encodeURI(
-              uploadedURL
-            )}`}
-          />
-        </div>
-      )} */}
+            <div className="mt-6">
+                {uploadedURL && <p className="mx-5 font-bold mb-2">Your current resume:</p>}
+                {uploadedURL && <div className="mx-5">
+                    <PDFViewer pdfURL={uploadedURL} />
+                </div>}
+            </div>
         </>
     );
 }
