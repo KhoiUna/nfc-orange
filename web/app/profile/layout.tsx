aaf9c55765614cd95a39f09127c2deaf1a5fc060
KhoiUna <thorwaitson@gmail.com>
2023-04-28 23:16:29 -0500

    modified:   web/app/profile/layout.tsx

--- web/app/profile/layout.tsx
+++ web/app/profile/layout.tsx
@@ -1,10 +1,15 @@
-'use client'
-
+import Analytics from "@/components/Analytics";
 import AppHeaderBar from "@/components/ui/AppHeaderBar";
+
+export const metadata = {
+    title: 'NFC Orange | Profile'
+}
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <>
+            <Analytics />
+
             <AppHeaderBar
                 title={'Profile'}
             />
