083005d0b7c68e737c2f595749a716cca6cf0295
KhoiUna <thorwaitson@gmail.com>
2023-05-01 10:39:59 -0500

    new file:   web/pages/api/homepage/index.ts

---
+++ web/pages/api/homepage/index.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "../../../db/client";
+import { ApiResponse } from "../register";
+
+export default async function view(
+    req: NextApiRequest,
+    res: NextApiResponse<ApiResponse>
+) {
+    try {
+        if (req.method !== "GET")
+            return res
+                .status(403)
+                .json({ success: false, error: "Method not allowed" });
+
+        const { rows } = await client.query('SELECT COUNT(*) FROM users')
+
+        return res.json({
+            success: rows[0],
+            error: false
+        })
+    } catch (error) {
+        console.error(error);
+        return res
+            .status(500)
+            .json({ success: false, error: "Internal server error" });
+    }
+}
