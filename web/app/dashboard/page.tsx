504c6ee35ce1750350b01c95a45872989192f9d6
KhoiUna <thorwaitson@gmail.com>
2023-05-05 18:28:02 -0500

    add `add link` feature

--- web/app/dashboard/page.tsx
+++ web/app/dashboard/page.tsx
@@ -7,9 +7,47 @@ import TextLoader from "@/components/ui/TextLoader";
 import greetUser from "@/lib/greetUser";
 import Link from "next/link";
 import AddLinkForm from "./components/AddLinkForm";
+import { Link as LinkType, User } from "../../types/types";
+import { useEffect, useState } from "react";
+import { Icon } from "@iconify/react";
+import { toast } from "react-hot-toast";
+import axios from "axios";
+
+type ApiResponse = {
+    success: {
+        user: User,
+        links: LinkType[]
+    };
+    error: any;
+}
+
+type LinkState = LinkType & { isSaved: boolean, isAdded: boolean }
+
+const linksInitialState: LinkState[] = [
+    {
+        isSaved: false,
+        isAdded: false,
+        link_title: '',
+        url: ''
+    }
+]
 
 export default function Dashboard() {
-    const { data, error } = useSWR("/api/profile", swrFetcher);
+    const { data, error } = useSWR<ApiResponse>("/api/profile", swrFetcher);
+
+    const [linkState, setLinkState] = useState<LinkState[]>(linksInitialState)
+    useEffect(() => {
+        setLinkState(linksInitialState)
+
+        if (data?.success) {
+            const newLinkState = data.success.links.filter(item => item.link_title !== 'My Resume').map(item => ({
+                ...item,
+                isSaved: true,
+                isAdded: false,
+            }))
+            setLinkState(prev => [...newLinkState, ...prev])
+        }
+    }, [data])
 
     if (error)
         return (
@@ -25,6 +63,45 @@ export default function Dashboard() {
             </div>
         );
 
+
+    const addLink = () => setLinkState(prev => [...prev, { ...linksInitialState[0], isAdded: true }])
+
+    const cancelLink = (index: number) => setLinkState((prev) => prev.filter((_, i) => i !== index))
+
+    const deleteLink = async (index: number, link_title: string, url: string) => {
+        try {
+            const { data } = await axios.delete(`/api/dashboard/link/?link_title=${link_title}&url=${url}`)
+
+            if (data.success) {
+                setLinkState((prev) => prev.filter((_, i) => i !== index))
+                toast.success('Link deleted!')
+            }
+        } catch (error) {
+            console.error('Error deleting link');
+            toast.error('Error deleting link')
+        }
+    }
+
+    // TODO: update link
+    // const updateLink = (link_title: string, url: string) => {
+    //     const newLinkState = linkState
+
+    //     const index = linkState.findIndex((item) => item.link_title === link_title && item.url === url)
+    //     newLinkState[index] = { ...newLinkState[index], isAdded: true }
+
+    //     console.log(newLinkState);
+
+    //     setLinkState(newLinkState)
+    // }
+
+    const saveLink = (link_title: string, url: string) => {
+        const newLinkState = [...linkState, { link_title, url, isAdded: false, isSaved: true }].map(item => ({
+            ...item, isAdded: false
+        }))
+        setLinkState(newLinkState)
+    }
+
+
     return (
         <div className="pb-5">
             <h2 className="text-xl mx-5 py-7">
@@ -36,16 +113,45 @@ export default function Dashboard() {
 
             <div className="mt-5 mx-3">
                 <Link href={`/resume`} className="block max-w-[500px] m-auto">
-                    <div className="border-2 border-black drop-shadow-lg p-3 rounded-lg bg-white text-center hover:bg-blue-100">
-                        <p className="font-bold">My Resume</p>
+                    <div className="font-bold border-2 border-black drop-shadow-lg p-3 rounded-lg bg-white text-center hover:bg-blue-100">
+                        My Resume
                     </div>
                 </Link>
 
-                {/* TODO: add social links */}
-                {/* <button className="block min-w-[500px] mx-auto mt-5 border-2 border-black drop-shadow-lg p-3 rounded-lg bg-white text-center hover:bg-blue-100">
-                    Add link
-                </button> */}
-                <AddLinkForm />
+                {
+                    linkState.map((item, index) => {
+                        if (item.isSaved && !item.isAdded) return (
+                            <button
+                                key={index}
+                                className="max-w-[500px] mx-auto flex justify-between items-center w-full font-bold mt-5 border-2 border-black drop-shadow-lg p-3 rounded-lg bg-white text-center hover:bg-blue-100"
+                            // onClick={() => updateLink(item.link_title, item.url)}
+                            >
+                                <div className="flex w-full justify-center">
+                                    {item.link_title}
+                                </div>
+
+                                <button
+                                    className="bg-red-100 ml-3 p-3 rounded-lg"
+                                    onClick={() => deleteLink(index, item.link_title, item.url)}
+                                >
+                                    <Icon className="text-2xl text-red-700" icon="material-symbols:delete-outline" />
+                                </button>
+                            </button>
+                        )
+
+                        return item.isAdded ? <AddLinkForm key={index} index={index} removeLink={cancelLink} saveLink={saveLink} link_title={item.link_title} url={item.url} /> : null
+                    })
+                }
+
+                {/* Only show `Add link` button when there's no current <AddLinkForm />  */}
+                {linkState.every(item => item.isAdded === false) &&
+                    <button
+                        className="bg-slate-100 text-slate-500 rounded-lg mt-5 border-2 border-slate-500 border-dashed p-3 block m-auto w-full max-w-[500px] hover:drop-shadow-lg hover:bg-blue-100"
+                        onClick={addLink}
+                    >
+                        + Add link
+                    </button>
+                }
             </div>
         </div>
     );
