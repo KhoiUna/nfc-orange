8930a30676627c605192b34fd0d59d40e8eafc23
KhoiUna <thorwaitson@gmail.com>
2023-05-04 20:12:01 -0500

    modified:   web/app/view/layout.tsx

--- web/app/view/layout.tsx
+++ web/app/view/layout.tsx
@@ -1,10 +1,9 @@
-import Link from "next/link";
 import { ReactNode } from "react";
-import Logo from "@/components/Logo";
 import Analytics from "@/components/Analytics";
 
 export const metadata = {
-    title: 'NFC Orange | View'
+    title: 'NFC Orange | View',
+    description: 'NFC Orange | View'
 }
 
 interface LayoutProps {
@@ -18,17 +17,7 @@ export default function ViewLayout({ children }: LayoutProps) {
         <>
             <Analytics />
 
-            <div>
-                <header className={`flex justify-center p-3 bg-primary`}>
-                    <Link href="/">
-                        <div className="w-[10rem] pt-2 h-fit cursor-pointer">
-                            <Logo />
-                        </div>
-                    </Link>
-                </header>
-
-                <main className="bg-slate-50 min-h-screen">{children}</main>
-            </div>
+            <main className="bg-slate-50 min-h-screen">{children}</main>
         </>
     )
 };
