245add9f6d44ed8df6d5713baf7e05f2175c8354
KhoiUna <thorwaitson@gmail.com>
2023-05-05 00:04:38 -0500

    modified:   web/app/dashboard/page.tsx

--- web/app/dashboard/page.tsx
+++ web/app/dashboard/page.tsx
@@ -6,27 +6,28 @@ import { swrFetcher } from "@/lib/swrFetcher";
 import TextLoader from "@/components/ui/TextLoader";
 import greetUser from "@/lib/greetUser";
 import Link from "next/link";
+import AddLinkForm from "./components/AddLinkForm";
 
 export default function Dashboard() {
     const { data, error } = useSWR("/api/profile", swrFetcher);
 
     if (error)
         return (
-            <div className="text-[1.8rem] text-center m-5">
+            <div className="text-[1.8rem] text-center mx-5 pt-5">
                 <h1>Failed to load</h1>
             </div>
         );
 
     if (!data)
         return (
-            <div className="text-[1.8rem] text-center m-5">
+            <div className="text-[1.8rem] text-center mx-5 pt-5">
                 <TextLoader loadingText="Loading" />
             </div>
         );
 
     return (
-        <>
-            <h2 className="text-xl mx-5 my-7">
+        <div className="pb-5">
+            <h2 className="text-xl mx-5 py-7">
                 {greetUser(data.success.user.first_name)}
             </h2>
 
@@ -39,7 +40,13 @@ export default function Dashboard() {
                         <p className="font-bold">My Resume</p>
                     </div>
                 </Link>
+
+                {/* TODO: add social links */}
+                {/* <button className="block min-w-[500px] mx-auto mt-5 border-2 border-black drop-shadow-lg p-3 rounded-lg bg-white text-center hover:bg-blue-100">
+                    Add link
+                </button> */}
+                <AddLinkForm />
             </div>
-        </>
+        </div>
     );
 }
