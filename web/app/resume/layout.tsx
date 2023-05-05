7c0a36b7121493d9a58a56ae6079f94fa8ba11e3
KhoiUna <thorwaitson@gmail.com>
2023-05-04 23:32:24 -0500

    modified:   web/app/resume/layout.tsx

--- web/app/resume/layout.tsx
+++ web/app/resume/layout.tsx
@@ -31,7 +31,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                 </nav>
             </header>
 
-            <main className="pt-8 max-h-screen overflow-auto">{children}</main>
+            <main className="pt-8 max-h-screen overflow-auto bg-slate-50">{children}</main>
         </>
     )
 }
