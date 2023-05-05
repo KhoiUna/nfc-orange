c32a2986b5fed0f0f709db1e06cb085701904990
KhoiUna <thorwaitson@gmail.com>
2023-05-04 20:57:26 -0500

    modified:   web/app/waitlist/layout.tsx

--- web/app/waitlist/layout.tsx
+++ web/app/waitlist/layout.tsx
@@ -1,20 +1,17 @@
 import HeaderBar from '@/components/ui/HeaderBar';
-import '@/styles/globals.css'
 
 export const metadata = {
     title: 'NFC Orange | Waitlist'
 }
 
-export default function RootLayout({ children }: {
+export default function WaitlistLayout({ children }: {
     children: React.ReactNode;
 }) {
     return (
         <html lang='en'>
             <HeaderBar title={metadata.title} />
 
-            <body>
-                <main>{children}</main>
-            </body>
+            <main>{children}</main>
         </html>
     )
 }
