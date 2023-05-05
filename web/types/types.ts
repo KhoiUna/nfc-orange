b30fe5aff958d7f024f6be38c295b3312dbad2e2
KhoiUna <thorwaitson@gmail.com>
2023-05-05 14:56:17 -0500

    modified:   web/types/types.ts

--- web/types/types.ts
+++ web/types/types.ts
@@ -10,6 +10,14 @@ export type Peer = {
 export type Student =
   | Peer
   | {
-      student_id: number;
-      uuid: string;
-    };
+    student_id: number;
+    uuid: string;
+  };
+
+export type User = {
+  major: string
+  first_name: string,
+  middle_name?: string,
+  last_name: string,
+  avatar_url?: string
+}
