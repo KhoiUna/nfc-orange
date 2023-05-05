d2920391eb601a50844363d2c8c812dd0de975d3
KhoiUna <thorwaitson@gmail.com>
2023-05-04 20:45:15 -0500

    modified:   web/app/layout.tsx

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({ children }: {
             <Analytics />
 
             <body>
-                <main>{children}</main>
+                {children}
             </body>
         </html>
     )
