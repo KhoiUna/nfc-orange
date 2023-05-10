9a25aaf390fda927c8a1b759becfa83c5c2091be
KhoiUna <thorwaitson@gmail.com>
2023-05-09 22:07:18 -0500

    modified:   web/styles/tailwind.ts

--- web/styles/tailwind.ts
+++ web/styles/tailwind.ts
@@ -1,3 +1,3 @@
-export const appSubmitButtonStyle = 'text-md bg-blue-100 rounded-lg p-3 flex items-center drop-shadow-lg m-auto mb-5 text-blue-800 active:drop-shadow-none'
+export const appSubmitButtonStyle = 'text-md bg-blue-100 rounded-lg p-3 flex items-center drop-shadow-lg m-auto mb-5 text-blue-800 cursor-pointer active:drop-shadow-none'
 
 export const inputStyle = "border-2 my-3 p-2 rounded-lg w-full drop-shadow-lg text-[1.3rem]"
