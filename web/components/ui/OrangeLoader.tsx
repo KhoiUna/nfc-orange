30c390579eba9284a018905e54c6999a8947eb60
KhoiUna <thorwaitson@gmail.com>
2023-05-13 00:34:54 -0500

    update OrangeLoader UI

--- web/components/ui/OrangeLoader.tsx
+++ web/components/ui/OrangeLoader.tsx
@@ -3,7 +3,7 @@ import TextLoader from "./TextLoader";
 
 export default function OrangeLoader() {
     return (
-        <div className="bg-[#FFFFE6] min-h-screen absolute top-0 left-0 right-0 w-full overflow-hidden">
+        <div className="bg-[#FFFFE6] min-h-[150vh] absolute top-0 left-0 right-0 w-full overflow-hidden">
             <Image className="m-auto pt-[25vh]" src={'/images/orange-loader.webp'} width={700} height={700} alt="NFC Orange loader" />
             <div className="text-lg font-bold text-center">
                 <TextLoader loadingText="Loading" />
