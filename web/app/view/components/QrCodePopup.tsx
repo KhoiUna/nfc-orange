fa2e890372f1fb6dbafd5506b3553cef98f7cd0b
KhoiUna <thorwaitson@gmail.com>
2023-05-19 17:05:32 -0500

    update QrCodePopup UI

--- web/app/view/components/QrCodePopup.tsx
+++ web/app/view/components/QrCodePopup.tsx
@@ -12,11 +12,11 @@ export default function QrCodePopup({ url, togglePopup }: Props) {
     return (
         <>
             <div
-                className="fixed bg-black opacity-[0.2] w-screen h-screen top-0 cursor-pointer"
+                className="fixed bg-black opacity-[0.5] w-screen h-screen top-0 cursor-pointer"
                 onClick={() => togglePopup()}
             />
 
-            <div className="fixed z-10 rounded-lg bg-white max-w-[500px] mx-5 sm:w-screen top-[25vh] left-0 right-0 p-4">
+            <div className="fixed z-10 rounded-lg bg-white max-w-[500px] mx-3 sm:m-auto sm:w-screen top-[25vh] left-0 right-0 p-4">
                 <p className="font-bold text-center pb-3">Scan QR code</p>
 
                 <Image
