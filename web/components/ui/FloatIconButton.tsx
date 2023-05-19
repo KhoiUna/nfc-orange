ec6a7f0605159a45b8cd497f2b67795213e4f85c
KhoiUna <thorwaitson@gmail.com>
2023-05-19 15:31:51 -0500

    update FloatIconButton

--- web/components/ui/FloatIconButton.tsx
+++ web/components/ui/FloatIconButton.tsx
@@ -1,21 +1,9 @@
-type Props = {
-    children: React.ReactNode
-    onClick: (event: React.SyntheticEvent) => void
-}
+import { ReactNode, ButtonHTMLAttributes } from 'react';
+
+type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
+    children: ReactNode;
+};
 
 export default function FloatIconButton(props: Props) {
-    const { children, onClick } = props
-
-    return (
-        <>
-            <button
-                className="bg-primary rounded-[100%] w-14 h-14 fixed right-2 bottom-2 drop-shadow-lg flex justify-center items-center"
-                onClick={(event) => onClick(event)}
-            >
-                {children}
-            </button>
-
-            <div className="pb-14" />
-        </>
-    )
+    return <button {...props}>{props.children}</button>
 }
