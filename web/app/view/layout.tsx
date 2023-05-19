0b8b56931ea2efa30c53851a01df8aef34d90d62
KhoiUna <thorwaitson@gmail.com>
2023-05-19 15:29:00 -0500

    remove HeaderBar

--- web/app/view/layout.tsx
+++ web/app/view/layout.tsx
@@ -1,5 +1,3 @@
-import HeaderBar from "@/components/ui/HeaderBar";
-
 export const metadata = {
     title: 'NFC Orange | View',
     description: 'NFC Orange | View'
@@ -8,11 +6,7 @@ export const metadata = {
 export default function ViewLayout({ children }: { children: React.ReactNode; }) {
 
     return (
-        <>
-            <HeaderBar title="Home" />
-
-            <main className="bg-slate-50 min-h-screen">{children}</main>
-        </>
+        <main className="bg-slate-50 min-h-screen">{children}</main>
     )
 };
 
