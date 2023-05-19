3967c9fc9d427c059fd921b407937d2d0bc435bc
KhoiUna <thorwaitson@gmail.com>
2023-05-19 12:16:24 -0500

    modified:   web/app/peers/head.tsx

--- web/app/peers/head.tsx
+++ web/app/peers/head.tsx
@@ -3,11 +3,11 @@ import Analytics from "@/components/Analytics";
 export default function head() {
     return (
         <>
-            <meta name="description" content="NFC ORANGE" />
+            <meta name="description" content="NFC Orange" />
             <link rel="icon" href="/nfc-orange.svg" />
             <link rel="shortcut icon" href="/nfc-orange.svg" type="image/x-icon" />
 
-            <title>NFC ORANGE | Peers</title>
+            <title>NFC Orange | Peers</title>
 
             <Analytics />
         </>
