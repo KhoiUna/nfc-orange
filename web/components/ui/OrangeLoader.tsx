af1341cec2573611302a7fa31d26295c47bc3c79
KhoiUna <thorwaitson@gmail.com>
2023-05-19 00:24:49 -0500

    modified:   web/components/ui/OrangeLoader.tsx

--- web/components/ui/OrangeLoader.tsx
+++ web/components/ui/OrangeLoader.tsx
@@ -4,7 +4,13 @@ import TextLoader from "./TextLoader";
 export default function OrangeLoader() {
     return (
         <div className="bg-[#FFFFE6] min-h-[150vh] absolute top-0 left-0 right-0 w-full overflow-hidden">
-            <Image priority className="drop-shadow-lg animate-bounce w-[100px] m-auto pt-[35vh]" src={'/images/orange-loader.png'} width={100} height={100} alt="NFC Orange loader" />
+            <Image
+                priority
+                className="drop-shadow-lg animate-bounce  w-auto h-auto m-auto pt-[35vh]"
+                src={'/images/orange-loader.png'}
+                width={60}
+                height={60}
+                alt="NFC Orange loader" />
 
             <div className="pt-5 text-lg font-bold text-center">
                 <TextLoader loadingText="Loading" />
