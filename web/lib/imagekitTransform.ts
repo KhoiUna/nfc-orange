76b43a4a014d21d065eaf552a2a205b61b4c5645
KhoiUna <thorwaitson@gmail.com>
2023-05-19 11:54:06 -0500

    modified:   web/lib/imagekitTransform.ts

--- web/lib/imagekitTransform.ts
+++ web/lib/imagekitTransform.ts
@@ -1,5 +1,5 @@
 const TRANSFORM_CONFIG = '?tr=w-120,r-max'
 
-export default function imagekitTransform(imageURL: string) {
-    return imageURL + TRANSFORM_CONFIG
+export default function imagekitTransform(imageURL: string | null) {
+    return imageURL ? imageURL + TRANSFORM_CONFIG : ''
 }
