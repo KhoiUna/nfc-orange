e711ddabbc3b38df90c7aea9f065a114e2997035
KhoiUna <thorwaitson@gmail.com>
2023-05-21 14:57:40 -0500

    update layout

--- web/app/view/layout.tsx
+++ web/app/view/layout.tsx
@@ -6,7 +6,7 @@ export const metadata = {
 export default function ViewLayout({ children }: { children: React.ReactNode; }) {
 
     return (
-        <main className="bg-slate-50 min-h-screen">{children}</main>
+        <main className="bg-slate-50 h-screen">{children}</main>
     )
 };
 
