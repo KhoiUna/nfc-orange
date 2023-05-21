49d48bcb9a3522453bd97678f95118842c375d66
KhoiUna <thorwaitson@gmail.com>
2023-05-21 11:03:12 -0500

    new file:   web/app/terms-of-service/page.tsx

---
+++ web/app/terms-of-service/page.tsx
@@ -0,0 +1,47 @@
+export default function TermsOfService() {
+    return (
+        <div>
+            <h1>Terms of Service</h1>
+            <p>
+                These Terms of Service ({"Terms"}) govern your use of the NFC Orange platform, including the website, mobile
+                applications, and related services (collectively referred to as the {"Service"}). By accessing or using the Service,
+                you agree to be bound by these Terms.
+            </p>
+
+            <h2>1. User Responsibilities</h2>
+            <p>
+                1.1 Eligibility: You must be at least 18 years old to use the NFC Orange platform. By using the Service, you
+                represent and warrant that you meet the age requirement.
+            </p>
+            <p>
+                1.2 Account: You are responsible for maintaining the confidentiality of your account information and for all
+                activities that occur under your account. Ensure that the information you provide is accurate, complete, and up to
+                date.
+            </p>
+
+            <h2>2. Intellectual Property</h2>
+            <p>
+                2.1 Ownership: NFC Orange retains all rights, titles, and interests in and to the Service and its content. The
+                Service is protected by copyright, trademark, and other intellectual property laws.
+            </p>
+            <p>
+                2.2 User Content: By using the Service, you grant NFC Orange a non-exclusive, worldwide, royalty-free license to
+                use, reproduce, modify, adapt, publish, distribute, and display the content you submit or provide through the
+                Service.
+            </p>
+
+            {/* Remaining sections... */}
+
+            <p>
+                Please review these Terms carefully. If you do not agree with any provision of these Terms, you should not access
+                or use the NFC Orange platform. By using the Service, you acknowledge and accept these Terms in their entirety.
+            </p>
+
+            <p>If you have any questions or concerns regarding these Terms, please contact us at [contact information].</p>
+
+            <p>Last updated: [Date]</p>
+
+            <p>NFC Orange Team</p>
+        </div>
+    );
+};
