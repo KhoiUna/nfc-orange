8132c4b07d25fe9a323796bf4802af32db4bcae2
KhoiUna <thorwaitson@gmail.com>
2023-05-23 12:37:06 -0500

    modified:   misc/send_mail.js

--- misc/send_mail.js
+++ misc/send_mail.js
@@ -2,14 +2,9 @@
 "use strict";
 
 require("dotenv").config();
-const hbs = require("nodemailer-express-handlebars");
-const nodemailer = require("nodemailer");
-const path = require("node:path");
 const { Client } = require("pg");
 const readline = require("node:readline/promises");
-const formData = require("form-data");
-const Mailgun = require("mailgun.js");
-const mailgun = new Mailgun(formData);
+const { sendMailgun } = require("./lib/email");
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -28,73 +23,6 @@ client
   .catch((error) => {
     console.error("Error connecting to database", error);
   });
-
-const sendMailgun = async (toAddress, subject, toName, emailTemplate) => {
-  try {
-    const mg = mailgun.client({
-      username: "api",
-      key: process.env.MAILGUN_API_KEY,
-    });
-
-    const MAILGUN_DOMAIN = "mg.nfcorange.com";
-
-    await mg.messages.create(MAILGUN_DOMAIN, {
-      from: `NFC Orange <contact@${MAILGUN_DOMAIN}>`,
-      to: [`${toName} <${toAddress}>`],
-      subject,
-      template: emailTemplate,
-      "h:X-Mailgun-Variables": JSON.stringify({ name: toName }),
-    });
-
-    return true;
-  } catch (error) {
-    console.error(error);
-    return;
-  }
-};
-
-const sendNodemailer = async (toAddress, subject, toName, emailTemplate) => {
-  try {
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: "nfcorange1@gmail.com",
-        pass: process.env.NODEMAILER_GMAIL_APP_PASSWORD,
-      },
-    });
-
-    // Point to the template folder
-    const handlebarOptions = {
-      viewEngine: {
-        partialsDir: path.resolve("./email_templates/"),
-        defaultLayout: false,
-      },
-      viewPath: path.resolve("./email_templates/"),
-    };
-    // Use a template file with nodemailer
-    transporter.use("compile", hbs(handlebarOptions));
-
-    // Send email
-    const info = await transporter.sendMail({
-      from: '"NFC Orange" <nfcorange1@gmail.com>',
-      to: toAddress,
-      subject,
-      template: emailTemplate,
-      context: {
-        subject,
-        name: toName, // replace {{name}} with `toName`
-      },
-    });
-    if (!info.messageId) throw new Error("Error sending email");
-
-    return info.messageId;
-  } catch (error) {
-    console.error(error);
-    return;
-  }
-};
 
 (async () => {
   const subject = process.argv[2];
