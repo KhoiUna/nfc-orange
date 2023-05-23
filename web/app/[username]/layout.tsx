7b810e5bf471ecd478999ed4dffd54e16285b89d
KhoiUna <thorwaitson@gmail.com>
2023-05-23 02:46:35 -0500

    use static metadata

--- web/app/[username]/layout.tsx
+++ web/app/[username]/layout.tsx
@@ -1,4 +1,7 @@
-import { Metadata } from 'next';
+export const metadata = {
+    title: 'NFC Orange | View',
+    description: 'NFC Orange | Get your digital business card & join our student community.'
+}
 
 export default function ViewLayout({ children }: { children: React.ReactNode; }) {
     return (
@@ -6,29 +9,31 @@ export default function ViewLayout({ children }: { children: React.ReactNode; })
     )
 };
 
-type Props = {
-    params: { username: string };
-};
+// TODO: generate dynamic metadata but page load is slow
+// import { Metadata } from 'next';
+// type Props = {
+//     params: { username: string };
+// };
 
-export async function generateMetadata(
-    { params }: Props,
-): Promise<Metadata> {
-    const { username } = params
+// export async function generateMetadata(
+//     { params }: Props,
+// ): Promise<Metadata> {
+//     const { username } = params
 
-    return {
-        title: `${username} | NFC Orange`,
-        description: `View @${username}'s profile on NFC Orange!`,
-        metadataBase: new URL('https://www.nfcorange.com'),
-        openGraph: {
-            images: [
-                {
-                    url: '/images/orange-loader.png',
-                    width: 556,
-                    height: 624,
-                },
-            ],
-            locale: 'en-US',
-            type: 'website',
-        }
-    };
-}
+//     return {
+//         title: `${username} | NFC Orange`,
+//         description: `View @${username}'s profile on NFC Orange!`,
+//         metadataBase: new URL('https://www.nfcorange.com'),
+//         openGraph: {
+//             images: [
+//                 {
+//                     url: '/images/orange-loader.png',
+//                     width: 556,
+//                     height: 624,
+//                 },
+//             ],
+//             locale: 'en-US',
+//             type: 'website',
+//         }
+//     };
+// }
