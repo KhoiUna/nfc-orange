b4cbc4e13a4f5bd4cd76b6e5e096531662c4bfcf
KhoiUna <thorwaitson@gmail.com>
2023-05-22 23:49:59 -0500

    modified:   sql-changelog/20230522.sql

--- sql-changelog/20230522.sql
+++ sql-changelog/20230522.sql
@@ -1,3 +1,13 @@
 -- DATE: 2023-05-22
 
 alter table users add column username varchar(45) unique;
+
+update users set first_name=trim(first_name), middle_name=trim(middle_name), last_name=trim(last_name);
+
+update users set first_name='Pahul',middle_name='Singh' where id=22;
+
+update users set middle_name='Le',last_name='Nohaic' where id=40;
+
+UPDATE users SET username=CONCAT(LOWER(first_name), LOWER(middle_name), LOWER(last_name));
+
+update users set username='yutraminoo' where id=42;
