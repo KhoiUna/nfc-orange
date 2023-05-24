fff78337f0a17491f2214028179de0fc69c6a9ca
KhoiUna <thorwaitson@gmail.com>
2023-05-23 20:58:05 -0500

    new file:   web/app/[username]/loading.tsx

---
+++ web/app/[username]/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return (
+        <div className="bg-[#FFFFE6] min-h-[150vh] absolute top-0 left-0 right-0 w-full overflow-hidden">
+            <p className="pt-[40vh] text-lg font-bold text-center">Loading...</p>
+        </div>
+    )
+}
