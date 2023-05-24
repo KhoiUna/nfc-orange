83ae64cc0e129853735f6cca9a6ef4ff37bfe85e
KhoiUna <thorwaitson@gmail.com>
2023-05-23 20:59:37 -0500

    update [username] layout.tsx to show dynamic metadata

--- web/app/[username]/layout.tsx
+++ web/app/[username]/layout.tsx
@@ -1,51 +1,56 @@
-export const metadata = {
-    title: 'Profile View | NFC Orange',
-    description: 'NFC Orange | Get your digital business card & join our student community.',
-    metadataBase: new URL('https://www.nfcorange.com'),
-    openGraph: {
-        images: [
-            {
-                url: '/images/orange-loader.png',
-                width: 556,
-                height: 624,
-            },
-        ],
-        locale: 'en-US',
-        type: 'website',
-    }
-}
+import client from '@/db/client';
+import { User } from '@/types/types';
+import { Metadata } from 'next';
 
 export default function ViewLayout({ children }: { children: React.ReactNode; }) {
-    return (
-        <main className="bg-slate-50 min-h-screen">{children}</main>
-    )
+    return <main className="bg-slate-50 min-h-screen">{children}</main>
 };
 
-// TODO: generate dynamic metadata but page load is slow
-// import { Metadata } from 'next';
-// type Props = {
-//     params: { username: string };
-// };
+type Props = {
+    params: { username: string };
+};
 
-// export async function generateMetadata(
-//     { params }: Props,
-// ): Promise<Metadata> {
-//     const { username } = params
+export async function generateMetadata(
+    { params }: Props,
+): Promise<Metadata> {
+    const { username } = params
 
-//     return {
-//         title: `${username} | NFC Orange`,
-//         description: `View @${username}'s profile on NFC Orange!`,
-//         metadataBase: new URL('https://www.nfcorange.com'),
-//         openGraph: {
-//             images: [
-//                 {
-//                     url: '/images/orange-loader.png',
-//                     width: 556,
-//                     height: 624,
-//                 },
-//             ],
-//             locale: 'en-US',
-//             type: 'website',
-//         }
-//     };
-// }
+    try {
+        const { rows } = await client.query<User>('SELECT first_name, middle_name, last_name, avatar_url FROM users WHERE username=$1', [username])
+        const { first_name, middle_name, last_name, avatar_url } = rows[0]
+
+        return {
+            title: `${first_name} ${middle_name} ${last_name} | NFC Orange`,
+            description: `View ${first_name} ${middle_name} ${last_name}'s profile on NFC Orange!`,
+            metadataBase: new URL('https://www.nfcorange.com'),
+            openGraph: {
+                images: [
+                    {
+                        url: avatar_url || '/images/orange-loader.png',
+                        width: 556,
+                        height: 624,
+                    },
+                ],
+                locale: 'en-US',
+                type: 'website',
+            }
+        };
+    } catch (error) {
+        return {
+            title: `@${username} | NFC Orange`,
+            description: `View @${username}'s profile on NFC Orange!`,
+            metadataBase: new URL('https://www.nfcorange.com'),
+            openGraph: {
+                images: [
+                    {
+                        url: '/images/orange-loader.png',
+                        width: 556,
+                        height: 624,
+                    },
+                ],
+                locale: 'en-US',
+                type: 'website',
+            }
+        };
+    }
+}
