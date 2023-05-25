02cb20e39f408b60ef8989c64192b574ff2a07c7
KhoiUna <thorwaitson@gmail.com>
2023-05-25 13:47:37 -0500

    modified:   web/pages/api/profile/avatar/auth.ts

--- web/pages/api/profile/avatar/auth.ts
+++ web/pages/api/profile/avatar/auth.ts
@@ -2,19 +2,17 @@ import { withIronSessionApiRoute } from "iron-session/next";
 import { sessionOptions } from "../../../../lib/session";
 import { NextApiRequest, NextApiResponse } from "next";
 import ImageKit from "imagekit";
-import { ApiResponse } from "../../register";
 
-async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<
-    | ApiResponse
-    | {
-        token: string;
-        expire: number;
-        signature: string;
-      }
-  >
-) {
+type ApiResponse = {
+  success: boolean
+  error: boolean | string
+} | {
+  token: string;
+  expire: number;
+  signature: string;
+}
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ApiResponse>) {
   if (!req.session.user?.isAuthenticated)
     return res.status(403).json({
       success: false,
