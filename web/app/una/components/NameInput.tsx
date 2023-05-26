1f156603210ba1f91aa23969b2307659c2615898
KhoiUna <thorwaitson@gmail.com>
2023-05-26 09:17:49 -0500

    fix localStorage bug

--- web/app/una/components/NameInput.tsx
+++ web/app/una/components/NameInput.tsx
@@ -1,22 +1,17 @@
 'use client'
 
 import { inputStyle } from "@/styles/tailwind";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function NameInput() {
     const [name, setName] = useState('')
-    const firstLoad = useRef(true)
 
     useEffect(() => {
-        if (firstLoad.current && localStorage.getItem('create_card_name')) setName(localStorage.getItem('create_card_name')!)
-
-        return () => {
-            firstLoad.current = false
-        }
+        if (localStorage.getItem('create_card_name')) setName(localStorage.getItem('create_card_name')!)
     }, [])
 
     useEffect(() => {
-        if (!firstLoad.current) localStorage.setItem('create_card_name', name)
+        localStorage.setItem('create_card_name', name)
     }, [name])
 
     return <input autoComplete="off" value={name} onChange={({ target }) => setName(target.value)} className={inputStyle + ' text-sm'} type="text" placeholder="Please type your full name" />
