eae6514258c43a1461890840f73585a4526989ca
KhoiUna <thorwaitson@gmail.com>
2023-05-25 22:26:22 -0500

    modified:   web/app/una/layout.tsx

--- web/app/una/layout.tsx
+++ web/app/una/layout.tsx
@@ -1,9 +1,19 @@
+import HeaderBar from "@/components/ui/HeaderBar"
+
 export const metadata = {
     title: 'Create your card | NFC Orange',
     description: 'NFC Orange | Create your own digital business card for University of North Alabama students.',
     viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
 }
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-    return <main>{children}</main>
+export default function CreateLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            <HeaderBar />
+
+            <main>
+                {children}
+            </main>
+        </>
+    )
 }
