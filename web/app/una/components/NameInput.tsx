f311c0fc68852f11da816f3279d4615cd9162c1c
KhoiUna <thorwaitson@gmail.com>
2023-05-25 22:54:23 -0500

    modified:   web/app/una/components/NameInput.tsx

--- web/app/una/components/NameInput.tsx
+++ web/app/una/components/NameInput.tsx
@@ -1,14 +1,22 @@
 'use client'
 
 import { inputStyle } from "@/styles/tailwind";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function NameInput() {
     const [name, setName] = useState('')
+    const firstLoad = useRef(true)
 
     useEffect(() => {
-        if (name) return localStorage.setItem('create_card_name', name)
-        if (localStorage.getItem('create_card_name')) return setName(localStorage.getItem('create_card_name')!)
+        if (firstLoad.current && localStorage.getItem('create_card_name')) setName(localStorage.getItem('create_card_name')!)
+
+        return () => {
+            firstLoad.current = false
+        }
+    }, [])
+
+    useEffect(() => {
+        if (!firstLoad.current) localStorage.setItem('create_card_name', name)
     }, [name])
 
     return <input autoComplete="off" value={name} onChange={({ target }) => setName(target.value)} className={inputStyle + ' text-sm'} type="text" placeholder="Please type your full name" />
