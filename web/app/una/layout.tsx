951acad56c516f07a525f718ea27d4a4595ee0a5
KhoiUna <thorwaitson@gmail.com>
2023-05-25 20:21:10 -0500

    new file:   web/app/una/layout.tsx

---
+++ web/app/una/layout.tsx
@@ -0,0 +1,9 @@
+export const metadata = {
+    title: 'Create your card | NFC Orange',
+    description: 'NFC Orange | Create your own digital business card for University of North Alabama students.',
+    viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return <main>{children}</main>
+}
