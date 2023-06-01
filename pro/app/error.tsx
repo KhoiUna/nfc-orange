d6ed513447ed44ad8df13cec19f1ce451b9ccab8
KhoiUna <thorwaitson@gmail.com>
2023-06-01 15:52:54 -0500

    modified:   pro/app/error.tsx

--- pro/app/error.tsx
+++ pro/app/error.tsx
@@ -1,8 +1,6 @@
 'use client'
 
-import HeaderBar from '@/components/ui/HeaderBar'
-import Image from 'next/image'
-import Link from 'next/link'
+import ErrorMessage from '@/components/ui/ErrorMessage'
 
 type Props = {
     error: Error
@@ -10,24 +8,5 @@ type Props = {
 }
 
 export default function Error({ error, reset }: Props) {
-    return (
-        <>
-            <HeaderBar />
-
-            <div id='gif-parallax' className="px-3 pt-[25vh] min-h-screen">
-                <h1 className='text-2xl text-center font-bold'>Oh no! Something goes wrong!</h1>
-                <h2 className='text-2xl text-center font-bold'>Click the image below to reset your session.</h2>
-
-                <Link href={'/api/logout'}>
-                    <Image
-                        className='m-auto'
-                        src={'/images/error.png'}
-                        alt='NFC Orange error image'
-                        width={300}
-                        height={300}
-                    />
-                </Link>
-            </div>
-        </>
-    );
+    return <ErrorMessage />
 }
