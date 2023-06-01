e53915aa8b0a1e897e681f216e34cf370b988dbe
KhoiUna <thorwaitson@gmail.com>
2023-06-01 16:24:03 -0500

    modified:   pro/app/analytics/page.tsx

--- pro/app/analytics/page.tsx
+++ pro/app/analytics/page.tsx
@@ -3,30 +3,56 @@
 import Chart from "chart.js/auto";
 import { CategoryScale } from "chart.js";
 import { Line } from "react-chartjs-2";
+import useSWR from "swr";
+import { swrFetcher } from "@/lib/swrFetcher";
+import OrangeLoader from "@/components/ui/OrangeLoader";
+import ErrorMessage from "@/components/ui/ErrorMessage";
 
 Chart.register(CategoryScale);
 
-const data = {
-    labels: ['Red', 'Orange', 'Blue'],
-    datasets: [
-        {
-            label: 'Profile view',
-            data: [55, 23, 96],
-            borderWidth: 1,
-        }
-    ]
+type ApiResponse = {
+    success: {
+        date: string
+        count: number
+    }[] | false
+    error: string | boolean
 }
 
 export default function Analytics() {
+    const { data: analyticsData, error } = useSWR<ApiResponse>(`/api/analytics`, swrFetcher)
+
+    if (error) return <ErrorMessage />
+
+    if (!analyticsData) return <OrangeLoader />
+
+    if (analyticsData.success === false) return <ErrorMessage />
+
+    const chartData = analyticsData.success.map(({ date, count }) => ({
+        date: new Date(date).toLocaleDateString(),
+        count
+    }))
+
     return (
         <Line
-            className="p-3 mt-3"
-            data={data}
+            className="p-3 pt-8"
+            data={{
+                labels: chartData.map(({ date }) => date),
+                datasets: [{
+                    label: 'Profile views',
+                    data: chartData.map(({ count }) => count),
+                    borderWidth: 2,
+                    borderColor: '#DC7700',
+                    backgroundColor: 'RGBA(220, 119, 0, 0.1)',
+                    fill: true,
+                    tension: 0.1
+                }]
+            }}
+
             options={{
                 plugins: {
                     title: {
                         display: true,
-                        text: "Users Gained between 2016-2020"
+                        text: "Profile Views"
                     },
                     legend: {
                         display: false
