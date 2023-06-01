aedd6c792fa3c032dd272c31c845a5747df4aeb5
KhoiUna <thorwaitson@gmail.com>
2023-06-01 16:49:48 -0500

    remove

--- pro/app/video/page.tsx
+++ pro/app/video/page.tsx
@@ -1,22 +1,17 @@
 'use client'
 
 import { swrFetcher } from "@/lib/swrFetcher";
-import PDFUpload from "./components/PDFUpload";
 import useSWR from "swr";
 import TextLoader from "@/components/ui/TextLoader";
 import useAuth from "@/lib/useAuth";
+import ErrorMessage from "@/components/ui/ErrorMessage";
 
 export default function Page() {
     useAuth({})
 
     const { data, error } = useSWR("/api/profile", swrFetcher);
 
-    if (error)
-        return (
-            <div className="text-[1.8rem] text-center m-5">
-                <h1>Failed to load</h1>
-            </div>
-        );
+    if (error) return <ErrorMessage />
 
     if (!data)
         return (
@@ -25,5 +20,9 @@ export default function Page() {
             </div>
         );
 
-    return <PDFUpload pdfURL={data.success.resume_link} />
+    return (
+        <>
+
+        </>
+    )
 }
