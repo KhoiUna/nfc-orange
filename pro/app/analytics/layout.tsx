178cc411a7bf74a44f511752532fd9a0f1f0dfed
KhoiUna <thorwaitson@gmail.com>
2023-06-01 16:31:26 -0500

    modified:   pro/app/analytics/layout.tsx

--- pro/app/analytics/layout.tsx
+++ pro/app/analytics/layout.tsx
@@ -1,14 +1,14 @@
 import AppHeaderBar from "@/components/ui/AppHeaderBar"
 
 export const metadata = {
-    title: 'Dashboard | NFC Orange',
+    title: 'Analytics | NFC Orange',
     description: 'NFC Orange | Get your digital business card & join our student community.'
 };
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+export default function AnalyticsLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
-            <AppHeaderBar title={'Dashboard'} />
+            <AppHeaderBar title={'Analytics'} />
 
             <main className="min-h-screen bg-slate-50">
                 {children}
