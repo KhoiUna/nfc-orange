d70d312588535e1ff1e9adb67d9b38850d262581
KhoiUna <thorwaitson@gmail.com>
2023-06-01 15:40:17 -0500

    update

--- pro/app/analytics/page.tsx
+++ pro/app/analytics/page.tsx
@@ -1,5 +1,38 @@
-export default function analytics() {
+'use client'
+
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import { Line } from "react-chartjs-2";
+
+Chart.register(CategoryScale);
+
+const data = {
+    labels: ['Red', 'Orange', 'Blue'],
+    datasets: [
+        {
+            label: 'Profile view',
+            data: [55, 23, 96],
+            borderWidth: 1,
+        }
+    ]
+}
+
+export default function Analytics() {
     return (
-        <>hi</>
+        <Line
+            className="p-3 mt-3"
+            data={data}
+            options={{
+                plugins: {
+                    title: {
+                        display: true,
+                        text: "Users Gained between 2016-2020"
+                    },
+                    legend: {
+                        display: false
+                    }
+                }
+            }}
+        />
     )
 }
