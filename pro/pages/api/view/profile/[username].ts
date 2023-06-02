0efa4c30b397bebe5eb6a15e6146bfc8b34c7515
KhoiUna <thorwaitson@gmail.com>
2023-06-01 19:41:15 -0500

    modified:   pro/pages/api/view/profile/[username].ts

--- pro/pages/api/view/profile/[username].ts
+++ pro/pages/api/view/profile/[username].ts
@@ -48,7 +48,7 @@ export default async function handler(
             );
 
             const { rows: user } = await client.query(
-                "SELECT first_name, middle_name, last_name, avatar_url, bio FROM users WHERE id=$1",
+                "SELECT first_name, middle_name, last_name, phone_number, avatar_url, banner_image_url, is_premium, bio FROM users WHERE id=$1",
                 [userId]
             );
 
