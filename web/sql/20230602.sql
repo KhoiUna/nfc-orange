ad8933fc0b14a3413dac6c0f65d42bed5d529483
KhoiUna <thorwaitson@gmail.com>
2023-06-02 23:21:25 -0500

    create table qr_histories

--- web/sql/20230602.sql
+++ web/sql/20230602.sql
@@ -9,3 +9,10 @@ alter table users add column banner_image_url TEXT;
 update users set is_premium =true where id=15;
 
 alter table scan_histories rename to card_tap_histories;
+
+create table qr_histories (
+    id serial primary key,
+    user_id int not null,
+    scanned_at timestamp without time zone not null,
+    foreign key(user_id) references users(id)
+);
