107f69e060caf67f28fe0bb29c6e0ed6264ab6d3
KhoiUna <thorwaitson@gmail.com>
2023-06-02 23:34:36 -0500

    modified:   pro/sql/20230602.sql

--- pro/sql/20230602.sql
+++ pro/sql/20230602.sql
@@ -6,3 +6,11 @@ create table qr_histories (
     scanned_at timestamp without time zone not null,
     foreign key(user_id) references users(id)
 );
+
+create table card_tap_histories (
+    id serial primary key,
+    card_id int not null,
+    scanned_at timestamp without time zone not null
+);
+
+alter table users add column card_id INT;
