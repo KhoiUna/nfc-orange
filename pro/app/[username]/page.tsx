36070500c2f35c011851881a67f4290071c860f5
KhoiUna <thorwaitson@gmail.com>
2023-06-02 22:15:40 -0500

    modified:   pro/app/[username]/page.tsx

--- pro/app/[username]/page.tsx
+++ pro/app/[username]/page.tsx
@@ -48,7 +48,7 @@ export default function View({ params }: Props) {
             const cookieOption = {
                 maxAge: 3600, // 1 hour
                 path: '/',
-                secure: process.env.NEXT_PUBLIC_PRODUCTION === "true",
+                secure: process.env.NODE_ENV === "production",
                 sameSite: true,
                 httpOnly: false,
             };
