54837336df35175c51d3ad2dcbf44fe635d227ae
KhoiUna <thorwaitson@gmail.com>
2023-06-04 16:56:24 -0500

    modified:   web/components/ui/AppHeaderBar.tsx

--- web/components/ui/AppHeaderBar.tsx
+++ web/components/ui/AppHeaderBar.tsx
@@ -19,10 +19,10 @@ const NAV_LINKS: NavLinkProps[] = [
     href: "/analytics",
     text: "Analytics",
   },
-  {
-    href: "/job-tools",
-    text: "Job Tools",
-  },
+  // {
+  //   href: "/job-tools",
+  //   text: "Job Tools",
+  // },
   // {
   //   href: "/peers",
   //   text: "Peers",
