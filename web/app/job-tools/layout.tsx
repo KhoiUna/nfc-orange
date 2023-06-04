b5d3b8a146c2f18cb8afa5cb1f3e9745a5d6d8c7
KhoiUna <thorwaitson@gmail.com>
2023-06-04 13:52:07 -0500

    add new page /job-tools

---
+++ web/app/job-tools/layout.tsx
@@ -0,0 +1,18 @@
+import AppHeaderBar from "@/components/ui/AppHeaderBar"
+
+export const metadata = {
+    title: 'Job Tools | NFC Orange',
+    description: 'NFC Orange | Get your digital business card & join our student community.'
+};
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            <AppHeaderBar title={'Dashboard'} />
+
+            <main className="min-h-screen bg-slate-50">
+                {children}
+            </main>
+        </>
+    );
+};
