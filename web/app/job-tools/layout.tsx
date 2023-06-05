3de948514c636cd403e2ca6022bc13a6761f026d
KhoiUna <thorwaitson@gmail.com>
2023-06-05 11:22:11 -0500

    update metadata of /job-tools page

--- web/app/job-tools/layout.tsx
+++ web/app/job-tools/layout.tsx
@@ -2,7 +2,24 @@ import HeaderBar from "@/components/ui/HeaderBar";
 
 export const metadata = {
     title: 'Job Tools | NFC Orange',
-    description: 'NFC Orange | Get your digital business card & join our student community.'
+    description: 'Job tools that power ⚡ your job search | NFC Orange',
+    metadataBase: new URL('https://www.nfcorange.com'),
+    themeColor: '#FFF0C3',
+    openGraph: {
+        title: 'Job Tools | NFC Orange',
+        description: 'Job tools that power ⚡ your job search | NFC Orange',
+        url: 'https://www.nfcorange.com/job-tools',
+        siteName: 'NFC Orange',
+        images: [
+            {
+                url: '/images/job-tools.png',
+                width: 512,
+                height: 512,
+            },
+        ],
+        locale: 'en-US',
+        type: 'website',
+    }
 };
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
