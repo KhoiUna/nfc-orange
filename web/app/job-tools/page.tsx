9406fbf36ea921d403f40a0b5a659e9ed9cab0cc
KhoiUna <thorwaitson@gmail.com>
2023-06-04 22:28:24 -0500

    update UI

--- web/app/job-tools/page.tsx
+++ web/app/job-tools/page.tsx
@@ -5,7 +5,9 @@ import Popup from "./components/Popup";
 export default function Page() {
     return (
         <>
-            <div className="py-8 flex flex-wrap justify-center items-center">
+            <h1 className="pt-7 pb-3 mx-3 text-3xl text-center font-bold">Job tools that power ⚡ your job search</h1>
+
+            <div className="pb-8 flex flex-wrap justify-center items-center">
                 {/* @ts-ignore*/}
                 {jobTools.map((item, index) => <JobToolCard key={index} jobItem={item} />)}
             </div>
