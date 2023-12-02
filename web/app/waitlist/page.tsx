0ebecdb02df669a91bb0b00905b15bbc37f98a16
KhoiUna <thorwaitson@gmail.com>
2023-12-02 12:15:14 -0600

    modified:   next.config.js

--- web/app/waitlist/page.tsx
+++ web/app/waitlist/page.tsx
@@ -2,32 +2,41 @@ import HeaderBar from "@/components/ui/HeaderBar";
 import Script from "next/script";
 
 export const metadata = {
-    title: 'NFC Orange | Waitlist',
-    description: 'NFC Orange | Modernize Career Fairs: Embrace the Paperless Revolution.'
-}
+	title: "NFC Orange | Waitlist",
+	description:
+		"NFC Orange | Modernize Career Fairs: Embrace the Paperless Revolution.",
+};
 
 export default function Waitlist() {
-    return (
-        <>
-            <HeaderBar />
+	return (
+		<>
+			<HeaderBar />
+			
 
-            <iframe
-                data-tally-src="https://tally.so/r/w7qGNa?transparentBackground=1"
-                width="100%"
-                height="100%"
-                title="NFC Orange | Onboarding"
-                style={{
-                    position: 'absolute',
-                    top: 0,
-                    right: 0,
-                    bottom: 0,
-                    left: 0,
-                    border: 0,
-                    background: '#fff'
-                }}
-            />
+			<h1 className="pl-5 pt-[10vh] text-xl">Sorry, we are closing our waitlist at the moment.</h1>
+			{/*
+			<iframe
+				data-tally-src="https://tally.so/r/w7qGNa?transparentBackground=1"
+				width="100%"
+				height="100%"
+				title="NFC Orange | Onboarding"
+				style={{
+					position: "absolute",
+					top: 0,
+					right: 0,
+					bottom: 0,
+					left: 0,
+					border: 0,
+					background: "#fff",
+				}}
+			/>
 
-            <Script async strategy={'afterInteractive'} src="https://tally.so/widgets/embed.js" />
-        </>
-    )
+			<Script
+				async
+				strategy={"afterInteractive"}
+				src="https://tally.so/widgets/embed.js"
+			/>
+			*/}
+		</>
+	);
 }
